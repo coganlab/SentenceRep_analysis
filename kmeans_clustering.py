141ae8c9afc0dd591993de96dc005844bb833e65
Aaronearlerichardson <jakdaxter31@gmail.com>
2022-06-10 18:25:10 -0400

    added tslearn fitting method to script

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -4,8 +4,9 @@ from sklearn.neighbors import NearestNeighbors
 from tslearn.clustering import TimeSeriesKMeans, silhouette_score
 import matplotlib.pyplot as plt
 
-x = np.load("A_LSwords+LMwords_aud+go.npy")
+x = np.load("Z_LSwords+LMwords_aud+go.npy")
 KMeans = TimeSeriesKMeans
+
 
 def cluster_variance(n):
     variances = []
@@ -18,27 +19,23 @@ def cluster_variance(n):
         kmeans.append(model)
         variances.append(model.inertia_)
 
-    return variances, K, n
+    return variances, K
 
 
-def calculate_WSS(kmax, repeat=10, points=x):
+def calculate_WSS(kmax, points=x):
     sse = []
-    for r in range(repeat):
-        sse_t=[]
-        for k in range(1, kmax + 1):
-            kmeans = KMeans(n_clusters=k).fit(points)
-            centroids = kmeans.cluster_centers_
-            pred_clusters = kmeans.predict(points)
-            curr_sse = 0
+    for k in range(1, kmax + 1):
+        kmeans = KMeans(n_clusters=k).fit(points)
+        centroids = kmeans.cluster_centers_
+        pred_clusters = kmeans.predict(points)
+        curr_sse = 0
 
-            # calculate square of Euclidean distance of each point from its cluster center and add to current WSS
-            for i in range(len(points)):
-                curr_center = centroids[pred_clusters[i]]
-                curr_sse += (points[i, 0] - curr_center[0]) ** 2 + (points[i, 1] - curr_center[1]) ** 2
+        # calculate square of Euclidean distance of each point from its cluster center and add to current WSS
+        for i in range(len(points)):
+            curr_center = centroids[pred_clusters[i]]
+            curr_sse += (points[i, 0] - curr_center[0]) ** 2 + (points[i, 1] - curr_center[1]) ** 2
 
-            sse_t.append(curr_sse)
-        sse.append(sse_t)
-    sse = np.mean(sse,0)
+        sse.append(curr_sse)
     return sse
 
 
@@ -52,6 +49,20 @@ def calc_sil(kmax):
     return sil
 
 
+def calc_score(kmax, method="sil", repeat=10):
+    k_scores = []
+    for r in range(repeat):
+        if method == "sil":
+            score = calc_sil(kmax)
+        elif method.lower() == "wss":
+            score = calculate_WSS(kmax)
+        elif method.lower() in ["var", "variance"]:
+            [score, _] = cluster_variance(kmax)
+        k_scores.append(score)
+    scores = np.mean(k_scores, 0)
+    return scores
+
+
 def sk_clustering(k):
     nbrs = NearestNeighbors(n_neighbors=k, algorithm='ball_tree').fit(x)
     #connectivity = nbrs.kneighbors_graph(x)
@@ -59,7 +70,6 @@ def sk_clustering(k):
         n_clusters=k, linkage="ward"#, connectivity=connectivity
     ).fit(x)
     return ward, nbrs
-
 
 
 def plot_clustering(labels, error=False):
@@ -74,22 +84,17 @@ def plot_clustering(labels, error=False):
             plt.plot(tscale,mean)
     plt.show()
 
+
 if __name__ == "__main__":
     n = 20
 
-    # variances, K, n = cluster_variance(30)
-    # plt.plot(K, variances)
-
-    sse = calculate_WSS(n, 50)
-    plt.plot(sse)
-    plt.ylabel("Within-Cluster Sum of Squared Values (WSS)")
-
-    # sil = calc_sil(n)
-    # plt.plot(sil)
+    # score = calc_score(n, "var", 20)
+    # plt.plot(score)
     # plt.ylabel("silhouette score")
-    plt.xlabel("K Value")
-    plt.xticks([i for i in range(1, n + 1)])
-    plt.show()
+    # plt.xlabel("K Value")
+    # plt.xticks([i for i in range(1, n + 1)])
+    # plt.show()
 
+    model = KMeans(n_clusters=6, random_state=82, verbose=2).fit(x)
     #ward, nbrs = sk_clustering(4)
-    #plot_clustering(ward.labels_)
+    plot_clustering(model.labels_)
