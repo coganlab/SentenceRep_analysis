8c75c6acc2bb79f107a030945886bc04444bbf8d
Aaronearlerichardson <jakdaxter31@gmail.com>
2022-08-23 22:13:26 -0400

    major changes to kmeans, doubling down on kmeans

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -1,11 +1,18 @@
 import numpy as np
-from sklearn.cluster import AgglomerativeClustering
-from tslearn.clustering import silhouette_score
+from sklearn.cluster import AgglomerativeClustering, FeatureAgglomeration, ward_tree
+from sklearn.decomposition import NMF
+from tslearn.clustering import silhouette_score, KernelKMeans, KShape
 from tslearn.clustering import TimeSeriesKMeans as KMeans
 from tslearn.neighbors import KNeighborsTimeSeries as NearestNeighbors
+from tslearn.barycenters import softdtw_barycenter
 import matplotlib.pyplot as plt
 
-x = np.load("../A_LSwords_aud+go_SM.npy")
+SM = np.load('../Z_LSwords_aud+resp_SM.npy')
+AUD = np.load('../Z_LSwords_aud+resp_AUD.npy')
+PROD = np.load('../Z_LSwords_aud+resp_PROD.npy')
+sigA = {'SM': np.load('../A_LSwords_aud+resp_SM.npy'),
+        'AUD': np.load('../A_LSwords_aud+resp_AUD.npy'),
+        'PROD': np.load('../A_LSwords_aud+resp_PROD.npy')}
 
 
 def cluster_variance(n):
@@ -22,51 +29,50 @@ def cluster_variance(n):
     return variances, K
 
 
-def calculate_WSS(kmax, points=x):
-    sse = []
-    for k in range(1, kmax + 1):
-        kmeans = KMeans(n_clusters=k).fit(points)
-        centroids = kmeans.cluster_centers_
-        pred_clusters = kmeans.predict(points)
-        curr_sse = 0
+def calculate_WSS(centroids, labels, points):
+    sse = 0
 
-        # calculate square of Euclidean distance of each point from its cluster center and add to current WSS
-        for i in range(len(points)):
-            curr_center = centroids[pred_clusters[i]]
-            curr_sse += (points[i, 0] - curr_center[0]) ** 2 + (points[i, 1] - curr_center[1]) ** 2
+    # calculate square of Euclidean distance of each point from its
+    # cluster center and add to current WSS
+    for i in range(len(points)):
+        curr_center = centroids[labels[i]]
+        sse += (points[i, 0] - curr_center[0]) ** 2 + \
+            (points[i, 1] - curr_center[1]) ** 2
 
-        sse.append(curr_sse)
     return sse
 
 
-def calc_sil(kmax):
-    # dissimilarity would not be defined for a single cluster, thus, minimum number of clusters should be 2
+def calc_sil(kmax, metric='euclidean'):
+    # dissimilarity would not be defined for a single cluster, thus, minimum
+    #  number of clusters should be 2
     sil = []
     for k in range(2, kmax + 1):
-        kmeans = KMeans(k).fit(x)
-        labels = kmeans.labels_
-        sil.append(silhouette_score(x, labels, metric='euclidean'))
+        kmeans = KMeans(k, metric=metric, n_init=5, n_jobs=-1, verbose=2)
+        labels = kmeans.fit_predict(x)
+        sil.append(silhouette_score(x, labels, metric=metric))
+    sil = [sil[0]] + sil
     return sil
 
 
-def calc_score(kmax, method="sil", repeat=10):
-    k_scores = []
-    for r in range(repeat):
-        if method.lower() in ["sil", "silhouette"]:
-            score = calc_sil(kmax)
-        elif method.lower() == "wss":
-            score = calculate_WSS(kmax)
-        elif method.lower() in ["var", "variance"]:
-            [score, _] = cluster_variance(kmax)
+def calc_score(X, kmax, model, metric='euclidean'):
+    sil = []
+    var = []
+    wss = []
+    for k in range(1, kmax + 1):
+        model.set_params(n_clusters=k)
+        labels = model.fit_predict(X)
+        if k == 1:
+            pass
         else:
-            raise SyntaxError("Scoring options are currently 'sil', 'var', and 'wss'")
-        k_scores.append(score)
-    scores = np.mean(k_scores, 0)
-    return scores, k_scores
+            sil.append(silhouette_score(X, labels, metric=metric))
+        var.append(model.inertia_)
+        wss.append(calculate_WSS(model.cluster_centers_,labels,X))
+    sil = [sil[0]] + sil
+    return sil, var, wss
 
 
 def sk_clustering(k):
-    nbrs = NearestNeighbors(n_neighbors=2, n_jobs=8).fit(x)
+    nbrs = NearestNeighbors(n_neighbors=2, n_jobs=-1).fit(x)
     connectivity = nbrs.kneighbors_graph(x)
     ward = AgglomerativeClustering(
         n_clusters=k, linkage="ward", connectivity=connectivity
@@ -74,7 +80,7 @@ def sk_clustering(k):
     return ward, nbrs
 
 
-def plot_clustering(labels, error=False):
+def plot_clustering(x, labels, error=False):
     for i in np.unique(labels):
         w_sigs = x[labels == i]
         if error:
@@ -95,24 +101,46 @@ def plot_std(mat: np.array):
     plt.errorbar(tscale, mean, yerr=std)
 
 
+def plot_opt_k(X, n, rep):
+    model = KMeans(n_jobs=-1, verbose=2)
+    methods = ['euclidean', 'dtw', 'softdtw']
+    scores = {}
+    for method in methods:
+        model.set_params(**{'metric': method})
+        sil = list(range(rep))
+        var = list(range(rep))
+        wss = list(range(rep))
+        for i in range(rep):
+            sil[i], var[i], wss[i] = calc_score(X, n, model, method)
+        score = {'sil': sil, 'var': var, 'wss': wss}
+        for key, value in score.items():
+            plot_std(value)
+            plt.ylabel("score")
+            plt.xlabel("K Value")
+            plt.xticks(np.array(range(n)), np.array(range(n))+1)
+            plt.title(method+"_"+key+"_"+str(len(X)))
+            plt.show()
+        scores[method] = score
+    return scores
+
+
 if __name__ == "__main__":
 
-    n = 20
-    rep = 40
-    xvals = []
+    for x,k in zip([SM, AUD, PROD],[4,4,3]):
+        scores = plot_opt_k(x, 10, 10)
+        model = KMeans(n_clusters=k, metric='softdtw', n_init=3, n_jobs=-1, verbose=2)
+        labels = model.fit_predict(x)
+        plot_clustering(x, labels, error=True)
 
-    for i in range(n):
-        xvals.append(np.array(range(n))+1)
-    for s_type in ['wss', 'sil', 'var']:
-        _, scores = calc_score(n, s_type, rep)
-        plot_std(scores)
-        # plt.scatter(xvals, scores)
-        plt.ylabel("score")
-        plt.xlabel("K Value")
-        plt.xticks(np.array(range(n)), np.array(range(n))+1)
-        plt.title(s_type)
-        plt.show()
+    #model = KMeans(n_clusters=4, metric='softdtw', n_init=4, n_jobs=-1, verbose=2)
+    # = KernelKMeans(n_clusters=4, n_jobs=-1, kernel="laplacian", verbose=2)
+    #model = KShape(n_clusters=4, verbose=2)
+    # model2 = NMF(n_components=4, init='random')
+    # W = model2.fit_transform(x)
+    # H = model2.components_
+    # model, nbrs = sk_clustering(4)
+    #model.fit(x)
+    #labels = model.fit_predict(x)
+    #plot_clustering(labels, error=True)
 
-    #model = KMeans(n_clusters=4, verbose=2).fit(x)
-    #model, nbrs = sk_clustering(4)
-    #plot_clustering(model.labels_, error=True)
+    #soft_dtw_graph(SM)
