f5f9b60f2ff91f00ee727514429c315904dd58e1
Aaronearlerichardson <jakdaxter31@gmail.com>
2022-10-20 01:44:26 -0400

    added stack 1st go

--- analysis.py
+++ analysis.py
@@ -10,20 +10,24 @@ from pandas import DataFrame as df
 Task, all_sigZ, all_sigA, sig_chans, sigMatChansLoc, sigMatChansName, Subject = load_all('data/whole.mat')
 
 cond = 'LSwords'
-aud = all_sigA[cond]["AuditoryWhole"]
-go = all_sigA[cond]["GoWhole"]
-audz = all_sigZ[cond]["AuditoryWhole"]
-goz = all_sigZ[cond]["GoWhole"]
+# resp = all_sigA[cond]["Response"]
+# respz = all_sigZ[cond]["Response"]
+# aud = all_sigA[cond]["AuditorywDelay"][:np.shape(resp)[0],:150]
+# audz = all_sigZ[cond]["AuditorywDelay"][:np.shape(resp)[0],:150]
+# go = all_sigA[cond]["DelaywGo"][:np.shape(resp)[0]]
+# goz = all_sigZ[cond]["DelaywGo"][:np.shape(resp)[0]]
 resp = all_sigA[cond]["ResponseWhole"]
 respz = all_sigZ[cond]["ResponseWhole"]
-sigConcat = np.concatenate([aud[:, 0:150],
-                            go[:, 150:400],
-                            resp[:, 150:400]], axis=1)
-newSet = [aud, go, resp, audz, goz, respz]
-sig_chans2 = list(set(np.concatenate(list(sig_chans[cond].values())).ravel().tolist()))
-sig_chans2 = [sig - 1 for sig in sig_chans2]
-nonActive = np.where(np.all(np.isclose(sigConcat, 0,), axis=1))
-newSet.append(sigConcat)
+aud = all_sigA[cond]["AuditoryWhole"]
+audz = all_sigZ[cond]["AuditoryWhole"]
+go = all_sigA[cond]["GoWhole"]
+goz = all_sigZ[cond]["GoWhole"]
+
+sigConcat = np.concatenate([aud,go,resp], axis=1)
+newSet = [aud, go, resp, audz, goz, respz, sigConcat]
+# sig_chans2 = list(set(np.concatenate(list(sig_chans[cond].values())).ravel().tolist()))
+# sig_chans2 = [sig - 1 for sig in sig_chans2]
+# nonActive = np.where(np.all(np.isclose(sigConcat, 0,), axis=1))
 for i, allign in enumerate(newSet):
     newSet[i] = newSet[i][np.any(sigConcat==1, axis=1)]
 [aud, go, resp, audz, goz, respz, sigConcat] = newSet[:]
@@ -36,25 +40,22 @@ sigSumWt = np.sum(np.array([audwt,gowt,respwt]), axis=0)
 sigSumZ = np.sum(np.array(newSet[3:6]), axis=0)
 
 # %% Generate the stitched signals
-stitched = stitch_mats([aud[:, 0:150], go[:, 150:400],
-                        resp[:, 150:400]], [0,0], axis=1)
-stitchedz = stitch_mats([audz[:, 0:150], goz[:, 150:400],
-                            respz[:, 150:400]], [0,0], axis=1)
-stitchedwt = stitch_mats([audwt[:, 0:150], gowt[:, 150:400],
-                            respwt[:, 150:400]], [0,0], axis=1)
+stitched = stitch_mats([aud, go, resp], [0,0], axis=1)
+stitchedz = stitch_mats([audz, goz, respz], [0,0], axis=1)
+stitchedwt = stitch_mats([audwt, gowt, respwt], [0,0], axis=1)
 # %% Data Plotting
-# plt.matshow(stitched)
+plt.matshow(stitched)
 # plt.plot(np.mean(stitched, axis=0))
 
 # %% Grid search decomposition
-x = to_sklearn_dataset(TimeSeriesScalerMinMax((0, 3)).fit_transform(stitched))
+x = to_sklearn_dataset(TimeSeriesScalerMinMax((0, 1)).fit_transform(sigSum[:,25:375]))
 gridsearch = estimate(x, NMF(max_iter=100000), 2)
 res = df(gridsearch.cv_results_)
 estimator = gridsearch.best_estimator_
 #estimator.n_components = 3
-y = estimator.transform(x)
+y = estimator.fit_transform(x)
 # %% decomposition plotting
-plot_weight_dist(x, y,["PROD", "SM", "AUD"],["blue","red","lime"])
+plot_weight_dist(x, y)#,["PROD", "SM", "AUD"],["blue","red","lime"])
 # plt.legend()
 # alt_plot(x,np.argmax(y,1))
 # decomp_sigs = np.dot(x.T, y)
@@ -64,3 +65,6 @@ gridsearch.scorer_ = gridsearch.scoring = {}
 np.save('data/gridsearch.npy', [gridsearch, x, y], allow_pickle=True)
 # plt.plot(decomp_sigs)
 plt.savefig('data/decomp.png')
+#%% load data
+# new = np.load('data/gridsearch.npy', allow_pickle=True).reshape(1)[0]
+# res = df(new[0].cv_results_)
