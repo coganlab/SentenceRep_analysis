0db6f9ec1bb61e0669810c8de3ab5a1163d2b4de
Aaronearlerichardson <jakdaxter31@gmail.com>
2023-05-10 17:52:53 -0400

    fixed outlier calc to be for absolute vals

--- check_chans.py
+++ check_chans.py
@@ -1,4 +1,5 @@
 from ieeg.viz.utils import chan_grid
+from ieeg.viz.parula import parula_map
 from ieeg.io import get_data, raw_from_layout, save_derivative
 from ieeg.navigate import trial_ieeg, channel_outlier_marker, crop_empty_data
 from ieeg.calc.scaling import rescale
@@ -13,7 +14,7 @@ if 'SLURM_ARRAY_TASK_ID' in os.environ.keys():
     subject = int(os.environ['SLURM_ARRAY_TASK_ID'])
 else:  # if not then set box directory
     LAB_root = os.path.join(HOME, "Box", "CoganLab")
-    subject = 8
+    subject = 29
 
 # Load the data
 TASK = "SentenceRep"
@@ -23,7 +24,7 @@ filt = raw_from_layout(layout.derivatives['clean'], subject=subj,
                        extension='.edf', desc='clean', preload=False)
 
 # %% fix SentenceRep events
-from mevents import fix_annotations  # noqa E402
+from events import fix_annotations  # noqa E402
 new = crop_empty_data(filt,)
 
 good = new.copy()
@@ -31,7 +32,8 @@ fix_annotations(good)
 
 # %% Crop raw data to minimize processing time
 
-good.info['bads'] = channel_outlier_marker(good, 3, 2)
+good.drop_channels(good.info['bads'])
+good.info['bads'] += channel_outlier_marker(good, 3, 2)
 good.drop_channels(good.info['bads'])
 good.info['bads'] += channel_outlier_marker(good, 4, 2)
 good.drop_channels(good.info['bads'])
@@ -45,13 +47,13 @@ del new
 # good.plot()
 
 # %% epoching
-resp = trial_ieeg(good, "Word/Response", (-2, 2), preload=True, outliers=10)
-base = trial_ieeg(good, "Start", (-1.5, 0.5), preload=True, outliers=10)
+resp = trial_ieeg(good, "Word/Response", (-1.5, 1.5), preload=True, outliers=10)
+base = trial_ieeg(good, "Start", (-1, 0.5), preload=True, outliers=10)
 
 # %% create spectrograms
-freqs = np.geomspace(2, 500, 100)
-# cyc = np.log(freqs) + 1.2
-cyc = np.linspace(0.5, 30, len(freqs))
+freqs = np.geomspace(2, 1000, 100)
+cyc = np.log(freqs)+1
+# cyc = np.linspace(0.5, 50, len(freqs))
 #
 
 base_s = tfr_morlet(base, freqs, n_jobs=7, verbose=10, average=True,
@@ -67,11 +69,13 @@ spec._data = rescale(resp_s._data, base_s._data, mode='ratio', axis=2,
 
 # %% plotting
 
-figs = chan_grid(spec, size=(16, 12), vmin=0.7, vmax=1.4, show=False)
+figs = chan_grid(spec, size=(16, 12), vmin=0.7, vmax=1.4, show=False,
+                 cmap=parula_map)
 for i, f in enumerate(figs):
     f.savefig(os.path.join(layout.root, 'derivatives', 'figs', 'wavelet',
                            f'{subj}_response_{i + 1}'))
 assert False
+
 # %% save bad channels
 filt.info['bads'] = spec.info['bads']
 save_derivative(filt, layout, 'clean', True)
