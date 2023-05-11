fixed trial averaging without outliers

check_chans.py
@@ -1,8 +1,9 @@
 from ieeg.viz.utils import chan_grid
 from ieeg.viz.parula import parula_map
-from ieeg.io import get_data, raw_from_layout, save_derivative
+from ieeg.io import get_data, raw_from_layout, update
 from ieeg.navigate import trial_ieeg, channel_outlier_marker, crop_empty_data
 from ieeg.calc.scaling import rescale
+from ieeg.calc.stats import avg_no_outlier
 import os
 from mne.time_frequency import tfr_morlet
 import numpy as np
@@ -14,7 +15,7 @@ if 'SLURM_ARRAY_TASK_ID' in os.environ.keys():
     subject = int(os.environ['SLURM_ARRAY_TASK_ID'])
 else:  # if not then set box directory
     LAB_root = os.path.join(HOME, "Box", "CoganLab")
-    subject = 29
+    subject = 9
 
 # Load the data
 TASK = "SentenceRep"
@@ -32,11 +33,11 @@ fix_annotations(good)
 
 # %% Crop raw data to minimize processing time
 
+# good.drop_channels(good.info['bads'])
+good.info['bads'] += channel_outlier_marker(good, 3, 2, save=True)
 good.drop_channels(good.info['bads'])
-good.info['bads'] += channel_outlier_marker(good, 3, 2)
-good.drop_channels(good.info['bads'])
-good.info['bads'] += channel_outlier_marker(good, 4, 2)
-good.drop_channels(good.info['bads'])
+# good.info['bads'] += channel_outlier_marker(good, 4, 2)
+# good.drop_channels(good.info['bads'])
 good.load_data()
 
 ch_type = filt.get_channel_types(only_data_chs=True)[0]
@@ -46,9 +47,14 @@ good.set_eeg_reference(ref_channels="average", ch_type=ch_type)
 del new
 # good.plot()
 
-# %% epoching
-resp = trial_ieeg(good, "Word/Response", (-1.5, 1.5), preload=True, outliers=10)
-base = trial_ieeg(good, "Start", (-1, 0.5), preload=True, outliers=10)
+# %% epoching and trial outlier removal
+avg_func = lambda x: avg_no_outlier(x, outliers=10)
+
+resp = trial_ieeg(good, "Word/Response", (-1.5, 1.5), preload=True)
+resp_avg = resp.average(None, avg_func)
+
+base = trial_ieeg(good, "Start", (-1, 0.5), preload=True)
+base_avg = base.average(None, avg_func)
 
 # %% create spectrograms
 freqs = np.geomspace(2, 1000, 100)
@@ -56,15 +62,17 @@ cyc = np.log(freqs)+1
 # cyc = np.linspace(0.5, 50, len(freqs))
 #
 
-base_s = tfr_morlet(base, freqs, n_jobs=7, verbose=10, average=True,
+base_s = tfr_morlet(base, freqs, n_jobs=7, verbose=10, average=False,
                     n_cycles=cyc, return_itc=False, decim=20, use_fft=True)
 base_s.crop(tmin=-0.5, tmax=0)
-resp_s = tfr_morlet(resp, freqs, n_jobs=7, verbose=10, average=True,
+resp_s = tfr_morlet(resp, freqs, n_jobs=7, verbose=10, average=False,
                     n_cycles=cyc, return_itc=False, decim=20, use_fft=True)
 resp_s.crop(tmin=-1, tmax=1)
 
-spec = resp_s.copy()
-spec._data = rescale(resp_s._data, base_s._data, mode='ratio', axis=2,
+resp_sa = resp_s.average(avg_func, copy=True)
+base_sa = base_s.average(avg_func, copy=True)
+spec = resp_sa.copy()
+spec._data = rescale(resp_sa._data, base_sa._data, mode='ratio', axis=2,
                      copy=True)
 
 # %% plotting
@@ -77,5 +85,4 @@ for i, f in enumerate(figs):
 assert False
 
 # %% save bad channels
-filt.info['bads'] = spec.info['bads']
-save_derivative(filt, layout, 'clean', True)
+update(good)