b512d5f3b78bd87fdbf798bb9705d07cc0e8c51f
Aaronearlerichardson <jakdaxter31@gmail.com>
2023-05-30 15:28:38 -0400

    check chans fixes

--- check_chans.py
+++ check_chans.py
@@ -1,25 +1,23 @@
-# Description: Check channels for outliers and remove them
+## Description: Check channels for outliers and remove them
 from ieeg.viz.utils import chan_grid
 from ieeg.viz.parula import parula_map
 from ieeg.io import get_data, raw_from_layout, update
-from ieeg.navigate import trial_ieeg, channel_outlier_marker, crop_empty_data
+from ieeg.navigate import trial_ieeg, channel_outlier_marker, crop_empty_data,\
+    outliers_to_nan
 from ieeg.calc.scaling import rescale
 from ieeg.calc.stats import avg_no_outlier, find_outliers
-import mpld3
-import pickle
 import os
-from mne.time_frequency import tfr_morlet
 from ieeg.timefreq.utils import wavelet_scaleogram
 import numpy as np
 
-# %% check if currently running a slurm job
+## check if currently running a slurm job
 HOME = os.path.expanduser("~")
 if 'SLURM_ARRAY_TASK_ID' in os.environ.keys():
     LAB_root = os.path.join(HOME, "workspace", "CoganLab")
     subject = int(os.environ['SLURM_ARRAY_TASK_ID'])
 else:  # if not then set box directory
     LAB_root = os.path.join(HOME, "Box", "CoganLab")
-    subject = 3
+    subject = 15
 
 # Load the data
 TASK = "SentenceRep"
@@ -28,17 +26,17 @@ layout = get_data("SentenceRep", root=LAB_root)
 filt = raw_from_layout(layout.derivatives['clean'], subject=subj,
                        extension='.edf', desc='clean', preload=False)
 
-# %% fix SentenceRep events
+## fix SentenceRep events
 from events import fix_annotations  # noqa E402
 new = crop_empty_data(filt,)
 
 good = new.copy()
 fix_annotations(good)
 
-# %% Crop raw data to minimize processing time
+## Crop raw data to minimize processing time
 
 # good.drop_channels(good.info['bads'])
-good.info['bads'] += channel_outlier_marker(good, 3, 2, save=True)
+good.info['bads'] += channel_outlier_marker(good, 3, 2)
 good.drop_channels(good.info['bads'])
 # good.info['bads'] += channel_outlier_marker(good, 4, 2)
 # good.drop_channels(good.info['bads'])
@@ -51,20 +49,20 @@ good.set_eeg_reference(ref_channels="average", ch_type=ch_type)
 del new
 # good.plot()
 
-# %% epoching and trial outlier removal
+## epoching and trial outlier removal
 
-resp = trial_ieeg(good, "Response", (-1.5, 1.5), preload=True)
+resp = trial_ieeg(good, "Word/Response", (-1.5, 1.5), preload=True)
 resp_gtrials = find_outliers(resp.get_data(), 11)
 resp_func = lambda x: avg_no_outlier(x, keep=resp_gtrials)
 # resp_avg = resp.average(None, resp_func)
 
 base = trial_ieeg(good, "Start", (-1, 0.5), preload=True)
-base_gtrials = find_outliers(base.get_data(), 8)
+base_gtrials = find_outliers(base.get_data(),8)
 base_func = lambda x: avg_no_outlier(x, keep=base_gtrials)
 # base_avg = base.average(None, base_func)
 # scaled = rescale(resp, base, mode='zscore', copy=True, outliers=10)
 
-# %% create spectrograms
+## create spectrograms
 
 resp_s = wavelet_scaleogram(resp, n_jobs=-2, decim=int(good.info['sfreq']/100))
 resp_s.crop(tmin=-1, tmax=1)
@@ -76,13 +74,13 @@ spec = rescale(resp_s, base_s, copy=True, mode='ratio',
 spec_a = spec.average(resp_func, copy=True)
 spec_a._data = np.log10(spec_a._data) * 20
 
-# %% plotting
+## plotting
 import matplotlib as mpl
-figs = chan_grid(spec_a, size=(16, 12), vmin=-2, vmax=2,
+figs = chan_grid(spec_a, size=(20, 10), vmin=-2, vmax=2,
                  cmap=parula_map, show=False)
 fig_path = os.path.join(layout.root, 'derivatives', 'figs', 'wavelet')
 for i, f in enumerate(figs):
-    f.savefig(os.path.join(fig_path, f'{subj}_response_{i + 1}'))
+    f.savefig(os.path.join(fig_path, f'{subj}_response_{i + 1}.jpg'), bbox_inches='tight')
     # pickle the figure for later
     # with open(os.path.join(fig_path, f'{subj}_response_{i + 1}.pkl'), 'wb'
     #           ) as file:
@@ -96,6 +94,6 @@ for i, f in enumerate(figs):
 # mpld3.show(html_fig)
 # assert False
 
-# %% save bad channels
-good.info['bads'] += spec.info['bads']
+## save bad channels
+good.info['bads'] += spec_a.info['bads']
 update(good, "muscle")
