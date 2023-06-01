ad02a2040e7aa890c8b4b773485cbd43bb1c21c1
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2023-06-01 14:33:38 -0400

    update submodules and scripts

--- MATLAB/channelOutlierRemoval.m
+++ MATLAB/channelOutlierRemoval.m
@@ -4,6 +4,7 @@ global TASK_DIR
 global experiment
 outlierSD=3;
 ieeg=trialIEEG(Trials,Subj.ChannelNums,Field,[-2000 5000]);
+ieeg=permute(ieeg,[2,1,3]);
 %ieegR=zeros(size(ieeg,2),size(ieeg,1)*size(ieeg,3));
 % for iChan=1:size(ieeg,2);
 %     disp([iChan '/' size(ieeg,2)]);
@@ -13,7 +14,6 @@ ieegR=reshape(ieeg,size(ieeg,1),size(ieeg,2)*size(ieeg,3));
 ieegR2=detrend(ieegR').^2;
 iiZero=find(ieegR2==0);
 ieegR2(iiZero)=.000000001;
-
 ieegSTD=std(log(ieegR2),[],1);
 ieegSTD=std(ieegR2,[],1);
 
@@ -22,6 +22,13 @@ iiOutPlus1=find(ieegSTD>(outlierSD*s+m));
 chanIn=setdiff(1:size(ieegSTD,2),iiOutPlus1);
 [m s]=normfit(ieegSTD(chanIn));
 iiOutPlus2=find(ieegSTD(chanIn)>(3*s+m));
+% chanIn=setdiff(chanIn,chanIn(iiOutPlus2));
+% [m s]=normfit(ieegSTD(chanIn));
+% iiOutPlus3=find(ieegSTD(chanIn)>(3*s+m));
+% chanIn=setdiff(chanIn,chanIn(iiOutPlus3));
+% [m s]=normfit(ieegSTD(chanIn));
+% iiOutPlus4=find(ieegSTD(chanIn)>(3*s+m));
+% chanIn=setdiff(chanIn,chanIn(iiOutPlus4));
 
 badChannels=sort(cat(2,iiOutPlus1,chanIn(iiOutPlus2)));
 
