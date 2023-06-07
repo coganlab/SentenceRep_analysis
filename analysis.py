5764d52bd93f76df67b8439c395ec80efee7de1b
Aaronearlerichardson <jakdaxter31@gmail.com>
2023-06-07 16:54:56 -0400

    added nmf method

--- analysis.py
+++ analysis.py
@@ -3,14 +3,15 @@ import mne
 import numpy as np
 from ieeg import PathLike, Doubles
 from ieeg.io import get_data
+from ieeg.viz.utils import plot_weight_dist
 from ieeg.viz.mri import get_sub_dir, plot_on_average
-from ieeg.calc.utils import stitch_mats
-import matplotlib.pyplot as plt
+from plotting import compare_subjects, plot_clustering
 from utils.mat_load import load_intermediates, group_elecs
-from copy import deepcopy, copy
+from copy import deepcopy
+from sklearn.decomposition import NMF
 
 
-class Analysis:
+class GroupData:
     def __init__(self, d_data: PathLike = None, task: str = "SentenceRep",
                  units: str = "uV", conditions: dict[str, Doubles] = None,
                  _zscore=None, _significance=None, _power=None, _epochs=None,
@@ -123,7 +124,7 @@ class Analysis:
         sig = new_data["sig"]
         zscore = new_data["zscore"]
 
-        return Analysis(self.root, self.task, self.units, self.conds, zscore,
+        return GroupData(self.root, self.task, self.units, self.conds, zscore,
                         sig, power, epochs, names)
 
     def get_condition(self, condition: str):
@@ -142,21 +143,67 @@ class Analysis:
         zscore = self.zscore[condition]
         conds = {condition: self.conds[condition]}
 
-        return Analysis(self.root, self.task, self.units, conds, zscore, sig,
+        return GroupData(self.root, self.task, self.units, conds, zscore, sig,
                         power, epochs, self.names)
 
-    def plot_groups_on_average(self):
+    def plot_groups_on_average(self, groups: list[list[int]] = None,
+                               colors: list[str] = ('red', 'green', 'blue')):
         assert hasattr(self, 'SM')
-        subjects = [v for v in self.epochs.values() if v]
-        if isinstance(subjects[0], dict):
-            cond = list(subjects[0].keys())[0]
-            subjects = [s[cond] for s in subjects]
-        brain = plot_on_average(subjects, picks=self.SM, color='red')
-        plot_on_average(subjects, picks=self.AUD, color='green', fig=brain)
-        plot_on_average(subjects, picks=self.PROD, color='blue', fig=brain)
+        cond = list(list(self.epochs.values())[0].keys())[0]
+        plot_data = self.get_condition(cond)
+        subjects = [v for v in plot_data.epochs.values() if v]
+        if groups is None:
+            groups = [self.SM, self.AUD, self.PROD]
+        itergroup = (g for g in groups)
+        if isinstance(colors, tuple):
+            colors = list(colors)
+        brain = plot_on_average(subjects, picks=next(itergroup),
+                                color=colors.pop(0))
+        for g, c in zip(itergroup, colors):
+            plot_on_average(subjects, picks=g, color=c, fig=brain)
         return brain
+
+    def get_training_data(self, dtype, conds, idx=None):
+        assert dtype in ['power', 'zscore', 'sig']
+        data = getattr(self, dtype)
+        if isinstance(data, dict):
+            data = list(data.get(cond) for cond in conds)
+            data = np.concatenate(data, axis=1)
+        elif isinstance(data, np.ndarray):
+            pass
+        else:
+            raise ValueError(f"Invalid type for data {type(data)}")
+
+        if idx is None:
+            idx = self.sig_chans
+        data = data[idx]
+        return data
+
+    def nmf(self, dtype: str = 'sig', n_components: int = 4,
+            idx: list[int] = None,
+            conds: list[str] = ('aud_ls', 'go_ls', 'resp'),
+            plot: bool = True, plot_dtype: str = None):
+        data = self.get_training_data(dtype, conds, idx)
+        data = data - np.min(data)
+        nmf = NMF(n_components=n_components, init='nndsvda', random_state=0,
+                  solver='mu', max_iter=1000, tol=1e-6)
+        W = nmf.fit_transform(data)
+        H = nmf.components_
+        if plot:
+            if plot_dtype is None:
+                plot_dtype = dtype
+            plot_data = self.get_training_data(plot_dtype, conds, idx)
+            plot_weight_dist(plot_data, W)
+            labels = np.argmax(W, axis=1)
+            groups = [[idx[i] for i in np.where(labels == j)[0]]
+                      for j in range(n_components)]
+            self.plot_groups_on_average(groups, colors=[
+                'blue', 'orange', 'green', 'red'])
+        return W, H
 
 
 if __name__ == "__main__":
-    data = Analysis()
-    data.plot_groups_on_average()
+    subject_data = GroupData()
+    W, H = subject_data.nmf(idx=subject_data.SM, plot_dtype='zscore')
+    # W, H = subject_data.nmf(n_components=3, plot_dtype='zscore')
+    # new_groups = subject_data.copy()
