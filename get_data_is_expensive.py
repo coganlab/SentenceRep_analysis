840f19bae0f7c27bc3f96a5b61724c00d5520b27
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2023-08-17 17:21:36 -0400

    simplify example
    Signed-off-by: Aaron Earle-Richardson <Jakdaxter31@gmail.com>

--- get_data_is_expensive.py
+++ get_data_is_expensive.py
@@ -1,28 +1,38 @@
 from timeit import timeit
 import mne
-from bids import BIDSLayout
-from ieeg.io import raw_from_layout
+from mne_bids import BIDSPath, read_raw_bids
 
-bids_root = mne.datasets.epilepsy_ecog.data_path()
-layout = BIDSLayout(bids_root)
-raw = raw_from_layout(layout, subject="pt1", preload=True, extension = ".vhdr", verbose = False)
+
+bids_path = BIDSPath(
+    root=mne.datasets.epilepsy_ecog.data_path(),
+    subject="pt1",
+    session="presurgery",
+    task="ictal",
+    datatype="ieeg",
+    extension=".vhdr",
+)
+raw = read_raw_bids(bids_path=bids_path, verbose="error")
 events, ids = mne.events_from_annotations(raw)
 trials = mne.Epochs(raw, events, event_id=ids, tmin=-10, tmax=10, preload=True, verbose=False)
 ids_rev = {v: k for k, v in ids.items()}
 trials.drop_bad()
 
 
-def iter_over_ch_ev(trials: mne.Epochs):
+def iter_over_ch_ev():
     for ch in trials.ch_names:
         for ev in events[:, 2]:
             return trials.get_data([ch], item=ids_rev[ev])
 
 
-def iter_over_array(trials):
+def iter_over_array():
     arr = trials.get_data()
     for i in range(arr.shape[1]):
         for j in range(arr.shape[0]):
             return arr[j, i]
 
-# %timeit iter_over_ch_ev(trials)
-# %timeit iter_over_array(trials)
+
+ch_ev = timeit(iter_over_ch_ev, number=1000)
+arr = timeit(iter_over_array, number=1000)
+
+print(f'iterating over mne object: {ch_ev:.4}s')
+print(f'iterating over array: {arr:.4}s')
