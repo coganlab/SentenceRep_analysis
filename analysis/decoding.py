3255fc8311e2d48dbf0d0c336b996b5981e49aa3
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2023-10-10 09:24:04 -0400

    sliding window works but slow
    Signed-off-by: Aaron Earle-Richardson <Jakdaxter31@gmail.com>

--- analysis/decoding.py
+++ analysis/decoding.py
@@ -28,12 +28,13 @@ class Decoder(PcaLdaClassification):
     def cv_cm(self, x_data: LabeledArray, kfolds: int = 5,
               repeats: int = 10, normalize: str = 'true', verbose: bool = True):
         rep = 0
-        n_cats = len(set(labels))
+        n_cats = len(self.categories)
         mats = np.zeros((repeats, kfolds, n_cats, n_cats))
         for f, (x_train, x_test, y_train, y_test) in enumerate(
                 self.splits(x_data, kfolds, 1)):
             fold = f + 1 - rep * kfolds
-            print("Fold {} of {}".format(fold, kfolds))
+            if verbose:
+                print("Fold {} of {}".format(fold, kfolds))
             self.fit(x_train, y_train)
             pred = self.predict(x_test)
             mats[rep, fold-1] = confusion_matrix(y_test, pred)
@@ -55,40 +56,49 @@ class Decoder(PcaLdaClassification):
                        normalize: str = 'true') -> np.ndarray:
         x_window = np.lib.stride_tricks.sliding_window_view(
             x_data, window_size, axis, subok=True)
+        labels = x_window.labels.pop(axis)
+        x_window.labels.extend(np.lib.stride_tricks.sliding_window_view(
+
+
+            labels, window_size, 0, subok=True).astype(str).decompose())
         windows_ax = axis - 1 if axis < 0 else axis
-        out = np.zeros((x_window.shape[windows_ax], 4, 4))
+        n_cats = len(self.categories)
+        out = np.zeros((x_window.shape[windows_ax], n_cats, n_cats))
         for i in tqdm(range(x_window.shape[windows_ax])):
-            in_data = np.take(x_window, i, windows_ax).reshape((x_data.shape[0], -1))
-            out[i] = self.cv_cm(in_data, labels, kfolds, repeats, normalize, False)
+            in_data = np.take(x_window, i, windows_ax)
+            out[i] = self.cv_cm(in_data, kfolds, repeats, normalize, False)
         return out
 
     def splits(self, x_data: LabeledArray, folds: int = 5, obs_axs: int = -2,
                smote: bool = True):
 
         obs_axs = list(range(x_data.ndim))[obs_axs]
-        non_trial_dims = tuple(i for i in range(x_data.ndim + 1) if i != obs_axs)
+        non_trial_dims = tuple(i for i in range(x_data.ndim) if i != obs_axs)
+        delim = x_data.labels[0].delimiter
+        data = x_data.combine((x_data.ndim - 2, x_data.ndim - 1))
 
-        f_idx = np.random.choice(np.arange(x_data.shape[obs_axs]),
-                                 (x_data.shape[obs_axs] // folds, folds),
+        f_idx = np.random.choice(np.arange(data.shape[obs_axs]),
+                                 (data.shape[obs_axs] // folds, folds),
                                  False)
-        f_data = np.take(x_data.__array__(), f_idx, obs_axs)
-        while np.sum(np.any(np.isnan(f_data), axis=non_trial_dims) == False) < 1:
-            f_idx = np.random.choice(np.arange(x_data.shape[obs_axs]),
-                                     (x_data.shape[obs_axs]//folds, folds),
+        while np.sum(np.any(np.isnan(np.take(data, f_idx, obs_axs)),
+                            axis=non_trial_dims) == False) < 2:
+            f_idx = np.random.choice(np.arange(data.shape[obs_axs]),
+                                     (data.shape[obs_axs]//folds, folds),
                                      False)
-            f_data = np.take(x_data.__array__(), f_idx, obs_axs)
 
         for j in range(folds):
             not_j = [i for i in range(folds) if i != j]
-            X_test = np.take(x_data, f_idx[..., j], obs_axs).combine((0, 1))
-            X_train = np.take(x_data, f_idx[..., not_j], obs_axs).combine((1, 2))
+            X_test = np.take(data, f_idx[..., j], obs_axs)
+            X_train = np.take(data, f_idx[..., not_j], obs_axs).combine((1, 2))
             if smote:
-                X_train = do_smote(X_test)
+                X_test = do_smote(X_test)
+                X_train = do_smote(X_train)
+            X_test = X_test.combine((0, 1))
             X_train = X_train.combine((0, 1))
             y_test = np.array(
-                [self.categories[k.split('-')[0]] for k in X_test.labels[0]])
+                [self.categories[k.split(delim)[0]] for k in X_test.labels[0]])
             y_train = np.array(
-                [self.categories[k.split('-')[0]] for k in X_train.labels[0]])
+                [self.categories[k.split(delim)[0]] for k in X_train.labels[0]])
             yield X_train.__array__(), X_test.__array__(), y_train, y_test
 
 
@@ -159,7 +169,7 @@ sub = GroupData.from_intermediates("SentenceRep", fpath, folder='stats_old')
 
 # %% Time Sliding decoding
 
-conds = ['aud_ls', 'aud_lm','aud_jl']
+conds = ['aud_ls']
 # idx = sub.AUD
 idxs = [sub.AUD, sub.SM, sub.PROD]
 colors = ['b', 'g', 'r']
@@ -167,7 +177,7 @@ scores = {'Auditory': None, 'Sensory-Motor': None, 'Production': None}
 for i, idx in enumerate(idxs):
     reduced = sub[:, conds][:, :, :, idx]
     reduced.array = reduced.array.dropna()
-    reduced = reduced.nan_common_denom(True, 7, False)
+    reduced = reduced.nan_common_denom(True, 8, False)
     # reduced.smotify_trials()
     comb = reduced.combine(('epoch', 'trial'))['power']
     x_data = (comb.array.dropna()
@@ -175,7 +185,7 @@ for i, idx in enumerate(idxs):
               ).swapaxes(1, 2)
 
     # Decoding
-    kfolds = 5
+    kfolds = 4
     repeats = 4
     mats = Decoder().sliding_window(x_data, 20, -1, kfolds,
                                     repeats, None)
@@ -185,25 +195,25 @@ for i, idx in enumerate(idxs):
               color=colors[i], label=list(scores.keys())[i])
 plt.legend()
 
-# %% Time Generalizing decoding
-
-conds = ['aud_ls', 'aud_lm','aud_jl']
-idx = sub.AUD
-reduced = sub[:, conds, :, idx, :]
-reduced.array = reduced.array.dropna()
-# reduced = reduced.nan_common_denom(True, 12, True)
-# reduced.smotify_trials()
-comb = reduced['power'].combine(('epoch', 'trial'))
-x_data = comb.array.dropna().swapaxes(0, 1)
-cats = {'heat': 1, 'hoot': 2, 'hot': 3, 'hut': 4}
-labels = np.array([cats[k.split('-')[0]] for k in x_data.labels[0]])
-
-# Decoding
-scorer = make_scorer(balanced_accuracy_score)
-SDecoder = mne.decoding.GeneralizingEstimator(PcaLdaClassification(), scorer)
-kfolds = 5
-repeats = 10
-cv = RepeatedStratifiedKFold(n_splits=kfolds, n_repeats=repeats)
-score = mne.decoding.cross_val_multiscore(
-    SDecoder, x_data.__array__(), labels, verbose=10, cv=cv, n_jobs=-1)
-plt.matshow(np.mean(score, axis=0), np.repeat(np.linspace(-0.5, 1.5, 200), 2))
+# # %% Time Generalizing decoding
+#
+# conds = ['aud_ls', 'aud_lm','aud_jl']
+# idx = sub.AUD
+# reduced = sub[:, conds, :, idx, :]
+# reduced.array = reduced.array.dropna()
+# # reduced = reduced.nan_common_denom(True, 12, True)
+# # reduced.smotify_trials()
+# comb = reduced['power'].combine(('epoch', 'trial'))
+# x_data = comb.array.dropna().swapaxes(0, 1)
+# cats = {'heat': 1, 'hoot': 2, 'hot': 3, 'hut': 4}
+# labels = np.array([cats[k.split('-')[0]] for k in x_data.labels[0]])
+#
+# # Decoding
+# scorer = make_scorer(balanced_accuracy_score)
+# SDecoder = mne.decoding.GeneralizingEstimator(PcaLdaClassification(), scorer)
+# kfolds = 5
+# repeats = 10
+# cv = RepeatedStratifiedKFold(n_splits=kfolds, n_repeats=repeats)
+# score = mne.decoding.cross_val_multiscore(
+#     SDecoder, x_data.__array__(), labels, verbose=10, cv=cv, n_jobs=-1)
+# plt.matshow(np.mean(score, axis=0), np.repeat(np.linspace(-0.5, 1.5, 200), 2))
