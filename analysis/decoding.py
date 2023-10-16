f732f953942b650eecfab05413897dc10af7eff0
Aaronearlerichardson <jakdaxter31@gmail.com>
2023-10-16 16:50:53 -0400

    Added repeater class, fixed oversampling test set, reorganized inputs to class vs function

--- analysis/decoding.py
+++ analysis/decoding.py
@@ -1,27 +1,30 @@
-# Decoding script, takes a GroupData set and uses a recurrent neural network to decode trial conditions
+# Decoding script, takes a GroupData set and uses Linear Discriminant Analysis
+# to decode trial conditions or word tokens from neural data
 
 import numpy as np
 import os
 
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, balanced_accuracy_score, make_scorer
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection._split import _RepeatedSplits
 import matplotlib.pyplot as plt
 
 from analysis.grouping import GroupData
 from IEEG_Pipelines.decoding.Neural_Decoding.decoders import PcaLdaClassification
 from ieeg.viz.utils import plot_dist
-from ieeg.calc.reshape import oversample_nan, smote as do_smote, norm, mixup
+from ieeg.calc.reshape import oversample_nan, norm, mixup
 from joblib import Parallel, delayed
 
 
 class AtLeastNDataPoints(StratifiedKFold):
-    """A RepeatedStratifiedKFold iterator that requires at least N non-nan data
-    points in each stratification of each fold"""
+    """A StratifiedKFold iterator that requires at least N non-nan data points
+    in each stratification of each fold"""
 
-    def __init__(self, n_splits, n_repeats=1, minimum_trials=1,
-                 random_state=None):
+    def __init__(self, n_splits, minimum_trials=1,
+                 random_state=None, shuffle=True):
         super().__init__(n_splits=n_splits,
-                         random_state=random_state)
+                         random_state=random_state,
+                         shuffle=shuffle)
         self.minimum_trials = minimum_trials
 
     def _iter_test_masks(self, X, y=None, groups=None):
@@ -30,7 +33,7 @@ class AtLeastNDataPoints(StratifiedKFold):
         while self._check_test_folds(X, y, test_folds) is False:
             test_folds = self._make_test_folds(X, y)
             i += 1
-            if i > 1000:
+            if i > 10000:
                 raise RecursionError('Could not find a valid split')
         for i in range(self.n_splits):
             yield test_folds == i
@@ -45,54 +48,69 @@ class AtLeastNDataPoints(StratifiedKFold):
         return True
 
 
+class Repeater(_RepeatedSplits):
+    """Implements a class that repeats a cross-validation strategy n times"""
+
+    def __init__(self, cv, *, n_splits=5, n_repeats=10, random_state=None):
+        super().__init__(cv,
+                         n_repeats=n_repeats,
+                         random_state=random_state,
+                         n_splits=n_splits)
+
+
 class Decoder(PcaLdaClassification):
 
     def __init__(self, *args,
-                 cv=StratifiedKFold,
-                 # cv=AtLeastNDataPoints,
+                 cv=AtLeastNDataPoints,
                  categories: dict = {'heat': 1, 'hoot': 2, 'hot': 3, 'hut': 4},
+                 n_splits: int = 5,
+                 n_repeats: int = 10,
+                 oversample: bool = True,
                  **kwargs):
         super().__init__(*args, **kwargs)
-        self.cv = cv
+        self.cv = Repeater(cv, n_splits=n_splits, n_repeats=n_repeats)
         self.categories = categories
 
-    def cv_cm(self, x_data: np.ndarray, labels: np.ndarray, kfolds: int = 5,
-              repeats: int = 10, smote: bool = True,
+        if not oversample:
+            self.oversample = lambda x: x
+        else:
+            self.oversample = lambda x, func: oversample_nan(x, func, False)
+
+    def cv_cm(self, x_data: np.ndarray, labels: np.ndarray,
               normalize: str = 'true', obs_axs: int = -2):
-        cv = self.cv(n_splits=kfolds)
+        cv = self.cv
         n_cats = len(set(labels))
         mats = np.zeros((repeats, kfolds, n_cats, n_cats))
         obs_axs = x_data.ndim + obs_axs if obs_axs < 0 else obs_axs
         idx = [slice(None) for _ in range(x_data.ndim)]
-        for rep in range(repeats):
-            for f, (train_idx, test_idx) in enumerate(cv.split(x_data.swapaxes(
-                    0, obs_axs), labels)):
-                x_train = np.take(x_data, train_idx, obs_axs)
-                x_test = np.take(x_data, test_idx, obs_axs)
-                y_train = np.take(labels, train_idx, 0)
-                y_test = np.take(labels, test_idx, 0)
-                if smote:
-                    for i in set(labels):
-                        idx[obs_axs] = y_train == i
-                        x_train[tuple(idx)] = oversample_nan(
-                            x_train[tuple(idx)], mixup, False)
-                        # fill in test data nans with noise from distribution
-                        # of existing test data
-                        # idx[obs_axs] = y_test == i
-                        # if any(np.isnan(x_test[tuple(idx)]).any(axis=2).all(
-                        #         axis=1)):
-                        #     raise ValueError(
-                        #         'Test data contains all nan trials')
-                        # x_test[tuple(idx)] = oversample_nan(
-                        #     x_test[tuple(idx)], norm, False)
+        for f, (train_idx, test_idx) in enumerate(cv.split(x_data.swapaxes(
+                0, obs_axs), labels)):
+            rep = f // kfolds
+            fold = f % kfolds
+            x_train = np.take(x_data, train_idx, obs_axs)
+            x_test = np.take(x_data, test_idx, obs_axs)
+            y_train = np.take(labels, train_idx, 0)
+            y_test = np.take(labels, test_idx, 0)
+            for i in set(labels):
+                # fill in train data nans with random combinations of
+                # existing train data trials (mixup)
+                idx[obs_axs] = y_train == i
+                x_train[tuple(idx)] = self.oversample(
+                    x_train[tuple(idx)], mixup)
 
-                x_test[np.isnan(x_test)] = np.random.normal(
-                    np.nanmean(x_test), np.nanstd(x_test),
-                    np.sum(np.isnan(x_test)))
+                # fill in test data nans with noise from distribution
+                # of existing test data
+                idx[obs_axs] = y_test == i
+                x_test[tuple(idx)] = self.oversample(
+                    x_test[tuple(idx)], norm)
 
-                self.fit(flatten_features(x_train, obs_axs), y_train)
-                pred = self.predict(flatten_features(x_test, obs_axs))
-                mats[rep, f] = confusion_matrix(y_test, pred)
+            # x_test[np.isnan(x_test)] = np.random.normal(
+            #     np.nanmean(x_test), np.nanstd(x_test),
+            #     np.sum(np.isnan(x_test)))
+
+            self.fit(flatten_features(x_train, obs_axs), y_train)
+            pred = self.predict(flatten_features(x_test, obs_axs))
+            mats[rep, fold] = confusion_matrix(y_test, pred)
 
         # average the repetitions, sum the folds
         mats = np.sum(mats, axis=1)
@@ -107,10 +125,9 @@ class Decoder(PcaLdaClassification):
             return mats
 
     def sliding_window(self, x_data: np.ndarray, labels: np.ndarray,
-                       window_size: int = 20, axis: int = -1, kfolds: int = 5,
-                       repeats: int = 10,
-                       obs_axs: int = -2, smote: bool = True,
-                       normalize: str = 'true', n_jobs: int = -3) -> np.ndarray:
+                       window_size: int = 20, axis: int = -1,
+                       obs_axs: int = -2, normalize: str = 'true',
+                       n_jobs: int = -3) -> np.ndarray:
 
         # make windowing generator
         axis = x_data.ndim + axis if axis < 0 else axis
@@ -125,8 +142,8 @@ class Decoder(PcaLdaClassification):
 
         # Use joblib to parallelize the computation
         gen = Parallel(n_jobs=n_jobs, return_as='generator', verbose=40)(
-            delayed(self.cv_cm)(x_data[idx], labels, kfolds, repeats,
-                                smote, normalize, obs_axs) for idx in idxs)
+            delayed(self.cv_cm)(x_data[idx], labels, normalize, obs_axs
+                                ) for idx in idxs)
         for i, mat in enumerate(gen):
             out[i] = mat
 
@@ -156,7 +173,7 @@ sub = GroupData.from_intermediates("SentenceRep", fpath, folder='stats_old')
 
 # %% Time Sliding decoding
 
-conds = ['go_ls', 'go_lm']
+conds = ['aud_lm']
 # idx = sub.AUD
 idxs = [sub.AUD, sub.SM, sub.PROD]
 colors = ['g', 'r', 'b']
@@ -166,7 +183,7 @@ for i, idx in enumerate(idxs):
     reduced = sub[:, conds][:, :, :, idx]
     reduced.array = reduced.array.dropna()
     # also sorts the trials by nan or not
-    reduced = reduced.nan_common_denom(True, 7, False)
+    reduced = reduced.nan_common_denom(True, 5, False)
     comb = reduced.combine(('epoch', 'trial'))['zscore']
     x_data = (comb.array.dropna()).combine((0, 2))
 
@@ -178,8 +195,9 @@ for i, idx in enumerate(idxs):
     # Decoding
     kfolds = 5
     repeats = 4
-    mats = Decoder().sliding_window(x_data.__array__(), labels, 20, -1, kfolds,
-                                    repeats, 1, True, 'true', 1)
+    decoder = Decoder(n_splits=kfolds, n_repeats=repeats, oversample=True)
+    mats = decoder.sliding_window(x_data.__array__(), labels, 20, -1, 1,
+                                  'true', 6)
     score = mats.T[np.eye(4).astype(bool)].T
     scores[list(scores.keys())[i]] = score.copy()
     if all(c == 'resp' for c in conds):
@@ -191,26 +209,5 @@ for i, idx in enumerate(idxs):
               color=colors[i], label=list(scores.keys())[i], ax=ax)
 plt.axhline(1/len(set(labels)), color='k', linestyle='--')
 plt.legend()
-
-# # %% Time Generalizing decoding
-#
-# conds = ['aud_ls', 'aud_lm','aud_jl']
-# idx = sub.AUD
-# reduced = sub[:, conds, :, idx, :]
-# reduced.array = reduced.array.dropna()
-# # reduced = reduced.nan_common_denom(True, 12, True)
-# # reduced.smotify_trials()
-# comb = reduced['power'].combine(('epoch', 'trial'))
-# x_data = comb.array.dropna().swapaxes(0, 1)
-# cats = {'heat': 1, 'hoot': 2, 'hot': 3, 'hut': 4}
-# labels = np.array([cats[k.split('-')[0]] for k in x_data.labels[0]])
-#
-# # Decoding
-# scorer = make_scorer(balanced_accuracy_score)
-# SDecoder = mne.decoding.GeneralizingEstimator(PcaLdaClassification(), scorer)
-# kfolds = 5
-# repeats = 10
-# cv = RepeatedStratifiedKFold(n_splits=kfolds, n_repeats=repeats)
-# score = mne.decoding.cross_val_multiscore(
-#     SDecoder, x_data.__array__(), labels, verbose=10, cv=cv, n_jobs=-1)
-# plt.matshow(np.mean(score, axis=0), np.repeat(np.linspace(-0.5, 1.5, 200), 2))
+plt.title("Listen-Mime")
+plt.ylim(0.1, 0.8)
