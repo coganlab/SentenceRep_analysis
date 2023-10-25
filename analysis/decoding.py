0b0ece072eac0dcb5baf86a553e9ae74789e84b5
Aaronearlerichardson <jakdaxter31@gmail.com>
2023-10-25 16:30:52 -0400

    new aggregate decoding comparison

--- analysis/decoding.py
+++ analysis/decoding.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 
 from analysis.grouping import GroupData
 from IEEG_Pipelines.decoding.Neural_Decoding.decoders import PcaLdaClassification
+from ieeg.calc.mat import LabeledArray
 from ieeg.viz.utils import plot_dist
 from ieeg.calc.oversample import oversample_nan, normnd as norm, mixupnd as mixup, TwoSplitNaN
 from joblib import Parallel, delayed
@@ -28,7 +29,7 @@ class Decoder(PcaLdaClassification):
         self.categories = categories
 
         if not oversample:
-            self.oversample = lambda x, *_: x
+            self.oversample = lambda x, func, ax: x
         else:
             self.oversample = lambda x, func, ax: oversample_nan(x, func, ax, False)
 
@@ -55,7 +56,8 @@ class Decoder(PcaLdaClassification):
                 # fill in test data nans with noise from distribution
                 # of existing test data
                 idx[obs_axs] = y_test == i
-                x_test[tuple(idx)] = self.oversample(x_test[tuple(idx)], norm, obs_axs)
+                x_test[tuple(idx)] = self.oversample(
+                    x_test[tuple(idx)], norm, obs_axs)
 
             # x_test[np.isnan(x_test)] = np.random.normal(
             #     np.nanmean(x_test), np.nanstd(x_test),
@@ -119,63 +121,76 @@ def classes_from_labels(labels: np.ndarray, delim: str = '-', which: int = 0,
     classes = {k: i for i, k in enumerate(np.unique(class_ids))}
     return classes, np.array([classes[k] for k in class_ids])
 
+
+def extract(sub: GroupData, conds: list[str], idx: list[int], common: int = 5,
+            datatype: str = 'zscore', crop_nan: bool = False) -> LabeledArray:
+    reduced = sub[:, conds][:, :, :, idx]
+    reduced.array = reduced.array.dropna()
+    # also sorts the trials by nan or not
+    reduced = reduced.nan_common_denom(True, common, crop_nan)
+    comb = reduced.combine(('epoch', 'trial'))[datatype]
+    return (comb.array.dropna()).combine((0, 2))
+
 # %% Imports
 
 fpath = os.path.expanduser("~/Box/CoganLab")
 sub = GroupData.from_intermediates("SentenceRep", fpath, folder='stats_old')
 all_scores = {}
+all_data = []
 
 # %% Time Sliding decoding
 
-conds = ['aud_ls']
+conds = ['aud_ls', 'aud_lm', 'aud_jl']
 # idx = sub.AUD
-idxs = [sub.AUD, sub.SM, sub.PROD]
 colors = ['g', 'r', 'b']
 scores = {'Auditory': None, 'Sensory-Motor': None, 'Production': None}
-fig, ax = plt.subplots()
-for i, idx in enumerate(idxs):
-    reduced = sub[:, conds][:, :, :, idx]
-    reduced.array = reduced.array.dropna()
-    # also sorts the trials by nan or not
-    reduced = reduced.nan_common_denom(True, 5, False)
-    comb = reduced.combine(('epoch', 'trial'))['zscore']
-    X = (comb.array.dropna()).combine((0, 2))
+names = list(scores.keys())
+fig, axs = plt.subplots(1, len(conds))
+if len(conds) == 1:
+    axs = [axs]
+for i, idx in enumerate([sub.AUD, sub.SM, sub.PROD]):
+    x_data = extract(sub, conds, idx, 9, 'zscore', True)
+    for cond, ax in zip(conds, axs):
+        X = x_data[:, cond]
+        all_data.append(X)
 
-    cats, labels = classes_from_labels(X.labels[1], crop=slice(0, 4))
-    # _, groups = classes_from_labels(x_data.labels[0], crop=slice(0, 5))
+        cats, labels = classes_from_labels(X.labels[1], crop=slice(0, 4))
 
-    # np.random.shuffle(labels)
+        # np.random.shuffle(labels)
 
-    # Decoding
-    kfolds = 5
-    repeats = 20
-    decoder = Decoder(n_splits=kfolds, n_repeats=repeats)
-    mats = decoder.sliding_window(X.__array__(), labels, 20, -1, 1,
-                                  'true', 7)
-    score = mats.T[np.eye(4).astype(bool)].T
-    scores[list(scores.keys())[i]] = score.copy()
-    if all(c == 'resp' for c in conds):
-        times = (-0.9, 0.9)
-    else:
-        times = (-0.4, 1.4)
-    pl_sc = scores[list(scores.keys())[i]]
-    plot_dist(np.reshape(pl_sc, (pl_sc.shape[0], -1)).T, times=times,
-              color=colors[i], label=list(scores.keys())[i], ax=ax)
-plt.axhline(1/len(set(labels)), color='k', linestyle='--')
-plt.legend()
-plt.title(conds[0])
-plt.ylim(0.1, 0.8)
-all_scores["-".join(conds)] = scores
+        # Decoding
+        kfolds = 5
+        repeats = 20
+        decoder = Decoder(n_splits=kfolds, n_repeats=repeats, oversample=False)
+        mats = decoder.sliding_window(X.__array__(), labels, 20, -1, 1,
+                                      'true', 7)
+        score = mats.T[np.eye(4).astype(bool)].T
+        scores[names[i]] = score.copy()
+        if cond == 'resp':
+            times = (-0.9, 0.9)
+        else:
+            times = (-0.4, 1.4)
+        pl_sc = scores[names[i]]
+        plot_dist(np.reshape(pl_sc, (pl_sc.shape[0], -1)).T, times=times,
+                  color=colors[i], label=list(scores.keys())[i], ax=ax)
+        all_scores["-".join([names[i], cond])] = score.copy()
+
+        if i == 2:
+            ax.axhline(1/len(set(labels)), color='k', linestyle='--')
+            ax.legend()
+            ax.set_title(cond)
+            ax.set_ylim(0.1, 0.8)
 
 # %% plot the electrode groups together
 fig, axs = plt.subplots(1, 3)
 # plot different conditions as different shade of the same color within group
 colors = ['g', 'r', 'b']
 colormap = {'r': [1, 0, 0], 'g': [0, 1, 0], 'b': [0, 0, 1]}
-for i, ax in enumerate(axs):
-    for j, (cond, elecs) in enumerate(all_scores.items()):
+for i, (ax, name) in enumerate(zip(axs, names)):
+    iterator = ((c, e) for c, e in all_scores.items() if c.startswith(name))
+    for j, (cond, elecs) in enumerate(iterator):
         color = list(max(min(k-0.5*(1-j) - 0.25, 1), 0) for k in colormap[colors[i]])
-        pl_sc = elecs[list(elecs.keys())[i]]
+        pl_sc = elecs[name]
         plot_dist(np.reshape(pl_sc, (pl_sc.shape[0], -1)).T,
                   times=times, color=color, label=cond, ax=ax)
     ax.axhline(1/len(set(labels)), color='k', linestyle='--')
