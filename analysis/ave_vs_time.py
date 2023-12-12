d0f21d432be3a2bb24b5db4cfcf387278af8f2d7
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2023-12-12 10:52:42 -0500

    small update

--- analysis/ave_vs_time.py
+++ analysis/ave_vs_time.py
@@ -7,12 +7,6 @@ import os
 fpath = os.path.expanduser("~/Box/CoganLab")
 sub = GroupData.from_intermediates("SentenceRep", fpath, folder='stats_old')
 
-# %% plotting
-# x = sub["zscore", "aud_ls", :, sub.AUD]
-# x.smotify_trials()
-# y = np.mean(x.combine(('stim', 'trial')).array,axis=0)
-# plot_dist(y.__array__(), times=(-0.5, 1.5))
-
 # %% Plot all channels
 x = sub.array["zscore", "aud_ls", :, sub.AUD]
 x.labels.insert(0, x.labels.pop(1))
