95f81d35f1aadfdcd1c933fc932b60676f00c656
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2023-12-15 00:41:24 -0500

    refactored
    Signed-off-by: Aaron Earle-Richardson <Jakdaxter31@gmail.com>

--- analysis/decoding.py
+++ analysis/decoding.py
@@ -65,8 +65,8 @@ class Decoder(eegdec.PcaLdaClassification):
 
             # fill in test data nans with noise from distribution
             # TODO: extract distribution from channel baseline
-            x_test[np.isnan(x_test)] = np.random.normal(
-                0, 1, np.sum(np.isnan(x_test)))
+            is_nan = np.isnan(x_test)
+            x_test[is_nan] = np.random.normal(0, 1, np.sum(is_nan))
 
             train_in = flatten_features(x_train, obs_axs)
             test_in = flatten_features(x_test, obs_axs)
@@ -167,38 +167,45 @@ def flatten_list(nested_list: list[list[str] | str]) -> list[str]:
             flat_list.append(element)
     return flat_list
 
+
+def concatenate_conditions(data, conditions, axis=1):
+    """Concatenate data for all conditions"""
+    concatenated_data = data[:, conditions[0]]
+    for condition in conditions[1:]:
+        concatenated_data = concatenated_data.concatenate(data[:, condition], axis=axis)
+    return concatenated_data
+
+
+def decode_and_score(decoder, data, labels, scorer, **decoder_kwargs):
+    """Perform decoding and scoring"""
+    mats, auc = decoder.sliding_window(data.__array__(), labels, **decoder_kwargs)
+    if scorer == 'acc':
+        score = mats.T[np.eye(len(decoder.categories)).astype(bool)].T
+    else:
+        score = auc
+    return score
+
+
 # %% Imports
 fpath = os.path.expanduser("~/Box/CoganLab")
-sub = GroupData.from_intermediates("SentenceRep", fpath, folder='stats_old')
+sub = GroupData.from_intermediates("SentenceRep", fpath, folder='stats_opt')
 # sub['power'].array = scale(sub['power'].array, np.max(sub['zscore'].array), np.min(sub['zscore'].array))
 all_scores = {}
 all_data = []
-
-# %% Time Sliding decoding
-
-conds = [['aud_ls', 'aud_lm'], ['go_ls', 'go_lm'], 'resp']
-# idx = sub.AUD
-colors = [[0, 1, 0], [1, 0, 0], [0, 0, 1]]
-scores = {'Auditory': None, 'Sensory-Motor': None, 'Production': None}
-idxs = [sub.AUD, sub.SM, sub.PROD]
-# colors = ['c', 'm', 'k', 'orange']
-# scores = {'Instructional': None, 'Motor': None, 'Feedback': None, 'Working Memory': None}
-# W = np.load('weights.npy')
-# groups = [[sub.SM[i] for i in np.where(np.argmax(W, axis=1) == j)[0]]
-#               for j in range(W.shape[1])]
-# idxs = groups
-# idxs = [sub.SM] * 4
+colors = [[0, 1, 0], [1, 0, 0], [0, 0, 1], [0.5, 0.5, 0.5]]
+scores = {'Auditory': None, 'Sensory-Motor': None, 'Production': None, 'All': None}
+idxs = [sub.AUD, sub.SM, sub.PROD, sub.sig_chans]
 idxs = [list(idx & sub.grey_matter) for idx in idxs]
 names = list(scores.keys())
+decoder = Decoder(0.8, 'lda', n_splits=5, n_repeats=10, oversample=True,)
+scorer = 'acc'
+window_kwargs = {'window_size': 20, 'axis': -1, 'obs_axs': 1, 'normalize': 'true', 'n_jobs': -3}
+
+# %% Time Sliding decoding for word tokens
+
+conds = [['aud_ls', 'aud_lm'], ['go_ls', 'go_lm'], 'resp']
 fig, axs = plt.subplots(1, len(conds))
 fig2, axs2 = plt.subplots(1, len(idxs))
-decoder = Decoder(0.8, 'qda', n_splits=5, n_repeats=10, oversample=True,
-                  DA_kwargs={
-                             'store_covariance': True,
-                             'tol': 1e-10})
-                             # 'covariance_estimator': skcov.OAS()})
-                  # , max_features=50*30)
-scorer = 'acc'
 if len(conds) == 1:
     axs = [axs]
     axs2 = [axs2] * len(idxs)
@@ -208,9 +215,7 @@ for i, (idx, ax2) in enumerate(zip(idxs, axs2)):
     ax2.set_title(names[i])
     for cond, ax in zip(conds, axs):
         if isinstance(cond, list):
-            X = x_data[:, cond[0]]
-            for c in cond[1:]:
-                X.concatenate(x_data[:, c], axis=1)
+            X = concatenate_conditions(x_data, cond)
             cond = "-".join(cond)
         else:
             X = x_data[:, cond]
@@ -220,12 +225,7 @@ for i, (idx, ax2) in enumerate(zip(idxs, axs2)):
         # np.random.shuffle(labels)
 
         # Decoding
-        mats, auc = decoder.sliding_window(X.__array__(), labels, 20, -1, 1,
-                                           'true', 6)
-        if scorer == 'acc':
-            score = mats.T[np.eye(4).astype(bool)].T# [acc_idx] / np.sum(mats, axis=-1)
-        else:
-            score = auc
+        score = decode_and_score(decoder, X, labels, scorer, **window_kwargs)
         scores[names[i]] = score.copy()
         if cond == 'resp':
             times = (-0.9, 0.9)
@@ -233,7 +233,7 @@ for i, (idx, ax2) in enumerate(zip(idxs, axs2)):
             times = (-0.4, 1.4)
         pl_sc = np.reshape(score.copy(), (score.shape[0], -1)).T
         plot_dist(pl_sc, times=times,
-                  color=colors[i], label=list(scores.keys())[i], ax=ax)
+                    color=colors[i], label=list(scores.keys())[i], ax=ax)
         plot_dist(pl_sc, times=times, label=cond, ax=ax2)
         all_scores["-".join([names[i], cond])] = score.copy()
 
@@ -247,34 +247,24 @@ for i, (idx, ax2) in enumerate(zip(idxs, axs2)):
     ax2.set_ylim(0.1, 0.8)
     ax2.axhline(1/len(set(labels)), color='k', linestyle='--')
 
-# %% plot the auditory and response aligned decoding
 
-# fig, axs = plt.subplots(1, 2)
-# conds = ['aud_ls', 'resp']
-# idx = sub.SM
-# x_data = extract(sub, conds, idx, 5, 'zscore', False)
-# for i, (cond, ax) in enumerate(zip(conds, axs)):
-#     X = x_data[:, cond]
-#     cats, labels = classes_from_labels(X.labels[1], crop=slice(0, 4))
-#     # np.random.shuffle(labels)
-#
-#     # Decoding
-#     mats, auc = decoder.sliding_window(X.__array__(), labels, 30, -1, 1,
-#                                   'true', 7)
-#     if scorer == 'acc':
-#         score = mats.T[np.eye(4).astype(bool)].T
-#     else:
-#         score = auc
-#     scores[names[i]] = score.copy()
-#     if cond == 'resp':
-#         times = (-0.9, 0.9)
-#     else:
-#         times = (-0.4, 1.4)
-#     pl_sc = np.reshape(score.copy(), (score.shape[0], -1)).T
-#     plot_dist(pl_sc, times=times, color='r', ax=ax)
-#     all_scores["-".join([names[i], cond])] = score.copy()
-#
-#     ax.axhline(1/len(set(labels)), color='k', linestyle='--')
-#     ax.set_title(cond)
-#     ax.set_ylim(0.1, 0.8)
+# %% Time Sliding decoding for conditions
 
+conds_aud = ['aud_ls', 'aud_lm', 'aud_jl']
+conds_go = ['go_ls', 'go_lm', 'go_jl']
+fig, ax = plt.subplots()
+for i, idx in enumerate(idxs):
+
+    # organize data
+    all_data = extract(sub, conds_aud + conds_go, idx, 5, 'zscore', False)
+    aud_data = concatenate_conditions(all_data, conds_aud)
+    go_data = concatenate_conditions(all_data, conds_go)
+    x_data = aud_data.concatenate(go_data, axis=2)
+    cats, labels = classes_from_labels(x_data.labels[1], crop=slice(-2, None), which=1)
+    decoder.categories = cats
+
+    # Decoding
+    score = decode_and_score(decoder, x_data, labels, scorer, **window_kwargs)
+    scores[names[i]] = score.copy()
+    pl_sc = np.reshape(score.copy(), (score.shape[0], -1)).T
+    plot_dist(pl_sc, times=(-0.4, 1.4), color=colors[i], label=list(scores.keys())[i], ax=ax)
