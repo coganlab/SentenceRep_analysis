4e0431b3798f8b410ec73630c3a51bcbc22079a6
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2024-02-16 01:44:24 -0500

    remove multitaper.py

--- analysis/check/multitaper_spec.py
+++ analysis/check/multitaper_spec.py
@@ -2,14 +2,11 @@
 import mne.time_frequency
 
 from ieeg.io import get_data, raw_from_layout
-from ieeg.navigate import trial_ieeg, channel_outlier_marker, crop_empty_data,\
-    outliers_to_nan
+from ieeg.navigate import trial_ieeg, crop_empty_data, outliers_to_nan
 from ieeg.calc.scaling import rescale
 import os
-from ieeg.timefreq.utils import wavelet_scaleogram, crop_pad
-from ieeg.timefreq.multitaper import spectrogram
+from ieeg.timefreq.utils import crop_pad
 import numpy as np
-import naplib.preprocessing as pre
 
 ## check if currently running a slurm job
 HOME = os.path.expanduser("~")
@@ -42,7 +39,7 @@ for sub in subjects:
 
     ## epoching and trial outlier removal
 
-    save_dir = os.path.join(layout.root, 'derivatives', 'spec', 'multitaper', sub)
+    save_dir = os.path.join(layout.root, 'derivatives', 'spec', 'multitaper_alt', sub)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
@@ -50,21 +47,26 @@ for sub in subjects:
             ("Start", "Start", "Word/Response/LS", "Word/Audio/LS", "Word/Audio/LM",
              "Word/Audio/JL", "Word/Go/LS", "Word/Go/LM",
              "Word/Go/JL"),
-            ((-0.5, 0), (-0.5, 0.5), (-1, 1), (-0.5, 1.5), (-0.5, 1.5), (-0.5, 1.5),
-             (-0.5, 1.5), (-0.5, 1.5), (-0.5, 1.5)),
+            ((-0.5, 0), (-0.5, 0.5), (-0.5, 1), (-0.5, 1.25), (-0.5, 1.25), (-0.5, 1.25),
+             (0, 1.5), (0, 1.5), (0, 1.5)),
             ("base", "start", "resp", "aud_ls", "aud_lm", "aud_jl", "go_ls", "go_lm",
              "go_jl")):
         t1 = t[0] - 0.5
         t2 = t[1] + 0.5
         trials = trial_ieeg(good, epoch, (t1, t2), preload=True)
-        outliers_to_nan(trials, outliers=20)
-        if name == "base":
-            base = trials.copy()
-            continue
+        outliers_to_nan(trials, outliers=10)
         freq = np.arange(10, 200., 2.)
-        spectra = spectrogram(trials, freq, base, n_jobs=6, pad="0.5s",
-                              n_cycles=freq / 2, time_bandwidth=20)
-        spec_a = spectra.copy()
+        kwargs = dict(average=False, n_jobs=-1, freqs=freq, return_itc=False,
+                      n_cycles=freq / 2, time_bandwidth=20)
+        if name == "base":
+            base = mne.time_frequency.tfr_multitaper(trials, **kwargs)
+            crop_pad(base, "0.5s")
+            continue
+
+        spectra = mne.time_frequency.tfr_multitaper(trials,  **kwargs)
+        crop_pad(spectra, "0.5s")
+        spec_a = rescale(spectra, base, copy=True, mode='ratio'
+                         ).average(lambda x: np.nanmean(x, axis=0), copy=True)
         fnames = [os.path.relpath(f, layout.root) for f in good.filenames]
         spec_a.info['subject_info']['files'] = tuple(fnames)
         spec_a.info['bads'] = good.info['bads']
