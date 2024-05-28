37f1440d54979bb4c5a78dfb4260c2d007881d56
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2024-05-28 18:17:15 -0400

    fix clean

--- analysis/fix/clean.py
+++ analysis/fix/clean.py
@@ -1,4 +1,4 @@
-##
+# %%
 import os
 from ieeg.io import get_data, raw_from_layout, save_derivative
 from ieeg.mt_filter import line_filter
@@ -13,22 +13,20 @@ else:  # if not then set box directory
     LAB_root = os.path.join(HOME, "Box", "CoganLab")
     subject = 36
 
-## Load Data
+# %% Load Data
 layout = get_data("SentenceRep", LAB_root)
-subjlist = layout.get_subjects()
-subjlist.sort()
-subj = subjlist[subject]
+subj = f"D{subject:04}"
 raw = raw_from_layout(layout, subject=subj, extension=".edf", desc=None,
                       preload=True)
 
-## filter data
-line_filter(raw, mt_bandwidth=10., n_jobs=-1, copy=False, verbose=10,
+# %% filter data
+line_filter(raw, mt_bandwidth=10., n_jobs=-3, copy=False, verbose=10,
             filter_length='700ms', freqs=[60], notch_widths=20)
 line_filter(raw, mt_bandwidth=10., n_jobs=-1, copy=False, verbose=10,
             filter_length='20s', freqs=[60, 120, 180, 240],
             notch_widths=20)
 
-## fix events
+# %% fix events
 fixed = fix(raw)
 
 # %% Save the data
