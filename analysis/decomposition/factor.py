3d16c3a4d50adf1d38b1dc3a7eaf92b8e8b364ee
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2024-07-12 11:06:00 -0400

    a mess

--- analysis/decomposition/factor.py
+++ analysis/decomposition/factor.py
@@ -1,13 +1,14 @@
 import torch
 from analysis.grouping import GroupData
-from ieeg.viz.ensemble import plot_dist, subgrids
+from ieeg.viz.ensemble import plot_dist, subgrids, plot_weight_dist
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import slicetca
 from multiprocessing import freeze_support
-import joblib
-from scipy.sparse import csr_matrix
+from scipy import stats as st
+from ieeg.calc.mat import LabeledArray
+from sklearn.preprocessing import minmax_scale
 
 # ## set up the figure
 fpath = os.path.expanduser("~/Box/CoganLab")
@@ -15,21 +16,32 @@ fpath = os.path.expanduser("~/Box/CoganLab")
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
 #
 #
+def mse(x, y, mask):
+    return (x - torch.masked_select(y, mask)) ** 2
 # %% Load the data
 
 if __name__ == '__main__':
     freeze_support()
     sub = GroupData.from_intermediates("SentenceRep", fpath, folder='stats')
-    idx = sorted(list(sub.SM))
+    # %%
+    idx = sorted(list(sub.SM & sub.grey_matter))
     zscore = np.nanmean(sub['zscore'].array, axis=(-4, -2))
     aud_slice = slice(0, 175)
+    pval = sub.p_vals
+    # pval = np.hstack([pval['aud_ls', :, aud_slice],
+                     # pval['resp']])
+    pval = np.where(pval > 0.9999, 0.9999, pval,)
+    zpval = LabeledArray(st.norm.ppf(1 - pval), zscore.labels)
     met = zscore
     trainz = np.hstack([met['aud_ls', :, aud_slice],
                         met['resp']])
     stitched = np.hstack([sub.signif['aud_ls', :, aud_slice],
                           # sub.signif['aud_lm', :, aud_slice],
                           sub.signif['resp', :]])
-    neural_data_tensor = torch.from_numpy(trainz[idx]).to(device)
+    data = trainz[idx]
+    neural_data_tensor = torch.from_numpy(data
+                                          / np.nanstd(data)
+                                          ).to(device)
 
     # Assuming neural_data_tensor is your 3D tensor
     # Remove NaN values
@@ -47,39 +59,43 @@ if __name__ == '__main__':
                                                 test_blocks_dimensions=(1, 5), # Same, 2*test_blocks_dimensions + 1
                                                 fraction_test=0.2,
                                                 device=device)
-    test_mask = torch.logical_and(test_mask, mask)
-    train_mask = torch.logical_and(train_mask, mask)
+    # test_mask = torch.logical_and(test_mask, mask)
+    # train_mask = torch.logical_and(train_mask, mask)
 
-    procs = 1
-    torch.set_num_threads(1)
+    procs = 3
+    torch.set_num_threads(2)
     min_ranks = [1]
     loss_grid, seed_grid = slicetca.grid_search(neural_data_tensor,
                                                 min_ranks = min_ranks,
                                                 max_ranks = [10],
-                                                sample_size=4,
+                                                sample_size=10,
                                                 mask_train=train_mask,
                                                 mask_test=test_mask,
                                                 processes_grid=procs,
-                                                seed=2,
+                                                seed=1,
                                                 min_std=10 ** -4,
                                                 learning_rate=5 * 10 ** -3,
                                                 max_iter=10**4,
-                                                positive=True)
-        # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
-    # np.savez('../loss_grid.npz', loss_grid=loss_grid, seed_grid=seed_grid,
-    #          idx=idx)
-    # slicetca.plot_grid(np.mean(loss_grid, axis=0), min_ranks=(0,))
-    # load the grid
-    # with np.load('../loss_grid.npz') as data:
-    #     loss_grid = data['loss_grid']
-    #     seed_grid = data['seed_grid']
-    # from ieeg.viz.ensemble import plot_dist;
+                                                positive=True,
+                                                batch_prop=1.0)
+    #     # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+    # # np.savez('../loss_grid.npz', loss_grid=loss_grid, seed_grid=seed_grid,
+    # #          idx=idx)
+    # # slicetca.plot_grid(np.mean(loss_grid, axis=0), min_ranks=(0,))
+    # # load the grid
+    # # with np.load('../loss_grid.npz') as data:
+    # #     loss_grid = data['loss_grid']
+    # #     seed_grid = data['seed_grid']
+    # # from ieeg.viz.ensemble import plot_dist;
+    # #
+    plot_dist(loss_grid.T)
+    plt.xticks(np.arange(0, 10), np.arange(1, 11))
     #
-    plot_dist(loss_grid.T[:, 1:])
-
-    # %% decompose the optimal model
-    n_components = (np.unravel_index(loss_grid.argmin(), loss_grid.shape) + np.array([0, 0]))[:-1]
-    best_seed = seed_grid[np.unravel_index(loss_grid.argmin(), loss_grid.shape)]
+    # # %% decompose the optimal model
+    # n_components = (np.unravel_index(loss_grid.argmin(), loss_grid.shape) + np.array([1, 0]))[:-1]
+    n_components = np.mean(loss_grid, axis=0).argmin() + 1
+    # best_seed = seed_grid[np.unravel_index(loss_grid.argmin(), loss_grid.shape)]
+    best_seed = seed_grid[n_components-1, loss_grid[n_components - 1].argmin()]
     # with torch.profiler.profile(
     #         schedule=torch.profiler.schedule(wait=0, warmup=1, active=1,
     #                                          repeat=0),
@@ -89,16 +105,19 @@ if __name__ == '__main__':
     #         activities=[torch.profiler.ProfilerActivity.CPU]
     # ) as prof:
     #     for i in range(2):
-    n_components = 6
-    losses, model = slicetca.decompose(neural_data_tensor, n_components,
+    # n_components = 4
+    losses, model = slicetca.decompose(neural_data_tensor, [n_components],
                                seed=best_seed,
                                positive=True,
                                min_std=10 ** -4,
                                learning_rate=5 * 10 ** -3,
-                               max_iter=10 ** 5
-                               )
-    slicetca.invariance(model)
+                               max_iter=10 ** 4,
+                               # mask=mask == 1
+                               batch_prop=1.0)
+    # slicetca.invariance(model)
             # prof.step()
+    W, H = model.get_components(numpy=True)[0]
+
 
     # %% plot the losses
     plt.figure(figsize=(4, 3), dpi=100)
@@ -110,3 +129,17 @@ if __name__ == '__main__':
     # %% plot the model
     axes = slicetca.plot(model,
                          variables=('neuron', 'time'),)
+
+    # %%
+    met = zpval
+    plotz = np.hstack([met['aud_ls', :, aud_slice],
+                        met['resp']])
+    fig = plot_weight_dist(plotz[idx], W.T)
+
+    # %%
+    # fig, axs = plt.subplots(2, 2)
+    size = minmax_scale(W, feature_range=(0.1, 1))
+    for i in range(W.shape[0]):
+        # ax = axs.flatten()[i]
+        size = W[i]*10
+        sub.plot_groups_on_average([idx], size=list(size), hemi='lh')
