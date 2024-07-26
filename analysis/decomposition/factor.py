b420b1dc3d624baadac15a5acc40bd8a65a08557
Aaronearlerichardson <jakdaxter31@gmail.com>
2024-07-26 04:20:07 -0400

    fix

--- analysis/decomposition/factor.py
+++ analysis/decomposition/factor.py
@@ -17,8 +17,6 @@ fpath = os.path.expanduser("~/Box/CoganLab")
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
 #
 #
-def mse(x, y):
-    return (x - y) ** 2
 # %% Load the data
 
 if __name__ == '__main__':
