3f94726b14de95245b7388d3e5af1faba1b25d83
Aaronearlerichardson <jakdaxter31@gmail.com>
2024-08-22 02:37:45 -0400

    try time

--- analysis/decoding/train.py
+++ analysis/decoding/train.py
@@ -43,10 +43,15 @@ class LabeledData(L.LightningDataModule):
         self.val_idx = self._fold_idx[fold]["val"]
         self.test_idx = self._fold_idx[fold]["test"]
 
-    def setup(self, stage=None):
-        cv = MinimumNaNSplit(self.folds, 1, which='test')
+    def setup(self, stage=None, rep=1):
+        cv = MinimumNaNSplit(self.folds, rep, which='test')
+        split = cv.split(self.data, self.targets)
+        while rep > 1:
+            for _ in range(self.folds):
+                next(split)
+            rep -= 1
         self._fold_idx = {}
-        for i, (train_idx, test_idx) in enumerate(cv.split(self.data, self.targets)):
+        for i, (train_idx, test_idx) in enumerate(split):
             train_idx = train_idx
             val_idx = train_idx[torch.distributions.uniform.Uniform(
                 0, 1).sample((train_idx.shape[0],)) < self.val_size]
@@ -84,8 +89,8 @@ device = ('cuda' if torch.cuda.is_available() else 'cpu')
 sub = GroupData.from_intermediates("SentenceRep", fpath, folder='stats')
 idx = sorted(list(sub.SM))
 aud_slice = slice(0, 175)
-# reduced = sub[:, :, :, idx][:, ['aud_ls', 'aud_lm', 'aud_jl']]
-reduced = sub[:, :, :, idx][:, ['go_ls', 'go_lm']]
+reduced = sub[:, :, :, idx][:, ['aud_ls', 'aud_lm', 'aud_jl']]
+# reduced = sub[:, :, :, idx][:, ['go_ls', 'go_lm']]
 
 reduced = reduced.nan_common_denom(True, 10, True)
 aud = reduced.array['zscore']
@@ -93,7 +98,7 @@ aud = reduced.array['zscore']
 idx = [i for i, l in enumerate(sub.array.labels[3]) if
  l in reduced.array.labels[2]]
 # transfer data to torch tensor
-aud.labels[0] = Labels(aud.labels[0].replace("go_", ""))
+aud.labels[0] = Labels(aud.labels[0].replace("aud_", ""))
 aud = LabeledArray(np.ascontiguousarray(aud.__array__()), aud.labels)
 data = aud.combine((0,1)).combine((0,2)).dropna()
 # aud_go = LabeledArray(np.ascontiguousarray(aud_go.__array__()), aud_go.labels)
@@ -110,28 +115,27 @@ neural_data_tensor = torch.from_numpy(
 # %% Create model
 # model parameters
 in_channels = data.shape[0]
-num_classes = 4
-d_model = data.shape[0] * data.shape[2]
-kernel_time = 50  # ms
+# num_classes = 4
+# d_model = data.shape[0] * data.shape[2]
+kernel_time = 25  # ms
 kernel_size = int(kernel_time * fs / 1000)  # kernel length in samples
-stride_time = 10  # ms
+stride_time = 5 # ms
 stride = int(stride_time * fs / 1000)  # stride length in samples
 padding = 0
 n_head = 6
 num_layers = 3
 dim_fc = 128
-dropout = 0.5
-learning_rate = 1e-5
+dropout = 0.3
+# learning_rate = 1e-5
 
 # instantiate the model
-model = SimpleDecoder(num_classes, d_model, learning_rate)
 # model = CNNTransformer(in_channels, num_classes, data.shape[1], kernel_size, stride, padding,
 #                           n_head, num_layers, dim_fc, dropout, learning_rate)
 
 # %% Train the model with kfold
 # instantiate the trainer
 n_folds = 5
-max_epochs = 20
+max_epochs = 500
 
 callbacks = [
              EarlyStopping(monitor='val_loss', patience=3, mode='min',min_delta=0.0001
@@ -142,49 +146,70 @@ trainer = L.Trainer(max_epochs=max_epochs,
                     logger=True,
                     )
 target_map = {'heat': 0, 'hut': 1, 'hot': 2, 'hoot': 3}
-dm = LabeledData(data, n_folds, val_size, target_map)
+
 
 # %% train the model
+def process_data(data, n_iters, n_folds, val_size,
+                 target_map, max_epochs, learning_rate = 1e-5):
+    d_model = data.shape[0] * data.shape[2]
+    dm = LabeledData(data, n_folds, val_size, target_map)
 
-n_iters = 6
-iter_accs = []
-gclip_val = 0.1
-max_epochs = 500
-es_pat = max_epochs // 20
-for i in range(n_iters):
-    dm.setup()
+    iter_accs = []
+    es_pat = max_epochs // 40
+    num_classes = len(target_map)
+    for i in range(n_iters):
+        dm.setup(rep=i + 1)
 
-    fold_accs = []
-    for fold in range(n_folds):
-        dm.set_fold(fold)
-        # print(dm.current_fold)
+        fold_accs = []
+        for fold in range(n_folds):
+            dm.set_fold(fold)
+            # print(dm.current_fold)
 
-        # instantiate the model
-        # in_channels = dm.get_data_shape()[-1]
-        model = SimpleDecoder(num_classes, d_model, learning_rate)
+            # instantiate the model
+            # in_channels = dm.get_data_shape()[-1]
+            # model = SimpleDecoder(num_classes, d_model, learning_rate)
+            model = CNNTransformer(in_channels, num_classes, d_model, kernel_size, stride, padding,
+                                      n_head, num_layers, dim_fc, dropout, learning_rate)
+            # model.current_fold = fold
+            callbacks = [
+                EarlyStopping(monitor='val_loss', patience=es_pat, mode='min', min_delta=0.01
+                              )]
+            trainer = L.Trainer(max_epochs=max_epochs,
+                                # gradient_clip_val=gclip_val,
+                                accelerator='auto',
+                                callbacks=callbacks,
+                                logger=False,
+                                enable_model_summary=False,
+                                enable_progress_bar=False,
+                                enable_checkpointing=False
+                                )
+            trainer.fit(model, dm)
+            # print(trainer.logged_metrics)
+            trainer.test(model, dm, verbose=False)
+            fold_accs.append(trainer.logged_metrics['test_acc'])
 
-        # model.current_fold = fold
-        callbacks = [
-            EarlyStopping(monitor='val_loss', patience=3, mode='min', min_delta=0.01
-                          )]
-        trainer = L.Trainer(max_epochs=max_epochs,
-                            # gradient_clip_val=gclip_val,
-                            accelerator='auto',
-                            callbacks=callbacks,
-                            logger=True,
-                            enable_model_summary=False,
-                            enable_progress_bar=True,
-                            )
-        trainer.fit(model, dm)
-        print(trainer.logged_metrics)
-        trainer.test(model, dm)
-        fold_accs.append(trainer.logged_metrics['test_acc'])
+            # save loss information
+            # loss_dict = trainer.logger.metrics
+            # loss_dict['fold'] = fold
+            # loss_dict['model'] = model
+        # print(f'Averaged accuracy: {sum(fold_accs) / len(fold_accs)}')
+        iter_accs.append(fold_accs)
+    # print(sum(iter_accs) / len(iter_accs), iter_accs)
+    # print(iter_accs)
+    return torch.as_tensor(iter_accs)
 
-        # save loss information
-        # loss_dict = trainer.logger.metrics
-        # loss_dict['fold'] = fold
-        # loss_dict['model'] = model
-    print(f'Averaged accuracy: {sum(fold_accs) / len(fold_accs)}')
-    iter_accs.append(fold_accs)
-# print(sum(iter_accs) / len(iter_accs), iter_accs)
-print(iter_accs)
+# %% windowed decoding
+from analysis.decoding import windower
+from joblib import Parallel, delayed
+
+data_windowed = LabeledArray(windower(data, 20, 2).swapaxes(0, -1))
+data_windowed.labels[1] = data.labels[0]
+data_windowed.labels[2] = data.labels[1]
+
+out = Parallel(n_jobs=4, verbose=40)(delayed(process_data)(
+    d, 5, n_folds, val_size, target_map, max_epochs) for d in data_windowed)
+
+# %% plot the results
+from ieeg.viz.ensemble import plot_dist
+plot = torch.stack(out).flatten(1).T
+fig = plot_dist(plot)
