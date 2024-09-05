a9232e9cc52b6759abd641e59b886a73f7e7ac0e
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2024-09-05 14:05:49 -0400

    remove unnecessary

--- analysis/decoding/words_hippocampal.py
+++ analysis/decoding/words_hippocampal.py
@@ -50,13 +50,12 @@ if __name__ == '__main__':
     sub = GroupData.from_intermediates(
         "SentenceRep", fpath, folder='stats', subjects_dir=subjects_dir)
     all_data = []
-    colors = [[0, 1, 0], [1, 0, 0], [0, 0, 1], [0.5, 0.5, 0.5]]
-    scores = {'Auditory': None, 'Sensory-Motor': None, 'Production': None, 'All': None}
-    scores2 = {'Auditory': None, 'Sensory-Motor': None, 'Production': None, 'All': None}
-    idxs = [sub.AUD, sub.SM, sub.PROD, sub.sig_chans]
+    colors = [[0, 1, 0]]
+    scores = {'Auditory': None}
+    idxs = [sub.AUD]
     idxs = [list(idx) for idx in idxs]
     names = list(scores.keys())
-    conds = [['aud_ls', 'aud_lm'], ['go_ls', 'go_lm'], 'resp']
+    conds = [['aud_ls', 'aud_lm']]
     window_kwargs = {'window': 20, 'obs_axs': 1, 'normalize': 'true', 'n_jobs': -2,
                     'average_repetitions': False}
 
@@ -66,11 +65,6 @@ if __name__ == '__main__':
                                 window_kwargs, scores,
                                 shuffle=False)
     dict_to_structured_array(scores, 'true_scores.npy')
-    # scores2 = score({'heat': 1, 'hoot': 2, 'hot': 3, 'hut': 4},
-    #                                 0.8, 'lda', 5, 250, sub, idxs, conds,
-    #                                 window_kwargs, scores2,
-    #                                 shuffle=True)
-    # dict_to_structured_array(scores2, 'shuffle_score.npy')
 
     # %% Plotting
     data_dir = ''
@@ -86,32 +80,3 @@ if __name__ == '__main__':
 
     for ax in fig.axes:
         ax.axhline(0.25, color='k', linestyle='--')
-
-    # # %% Time Sliding decoding significance
-    #
-    # shuffle_score = np.load(data_dir + 'shuffle_score_short.npy', allow_pickle=True)[0]
-    # shuffle_score = {name: shuffle_score[name] for name in shuffle_score.dtype.names}
-    # signif = {}
-    # for cond, score in scores.items():
-    #     true = np.mean(score.T[np.eye(4).astype(bool)].T, axis=2)
-    #     shuffle = np.mean(shuffle_score[cond].T[np.eye(4).astype(bool)].T, axis=2)
-    #     signif[cond] = time_perm_cluster(true.T, shuffle.T, 0.001, stat_func=lambda x, y, axis: np.mean(x, axis=axis))
-
-    # # %% Plot significance
-    # for cond, ax in zip(conds, axs):
-    #     bars = []
-    #     if isinstance(cond, list):
-    #         cond = "-".join(cond)
-    #     for i, idx in enumerate(idxs):
-    #         name = "-".join([names[i], cond])
-    #         if name.endswith('resp'):
-    #             times = (-1, 1)
-    #         else:
-    #             times = (-0.5, 1.5)
-    #         shuffle = np.mean(shuffle_score[name].T[np.eye(4).astype(bool)].T, axis=2)
-    #         # smooth the shuffle using a window
-    #         window = np.lib.stride_tricks.sliding_window_view(shuffle, 20, 0)
-    #         shuffle = np.mean(window, axis=-1)
-    #         plot_dist_bound(shuffle, 'std', 'both', times, 0, ax=ax, color=colors[i], alpha=0.3)
-    #         bars.append(signif[name])
-    #     plot_horizontal_bars(ax, bars, 0.05, 'below')
