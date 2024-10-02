fd4d71671b022d6e6854f503e68cc1718f7dd29b
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2024-10-02 12:26:24 -0400

    shorter

--- analysis/decomposition/slice.py
+++ analysis/decomposition/slice.py
@@ -192,7 +192,7 @@ if __name__ == '__main__':
         data = neural_data_tensor.mean(0).detach().cpu().numpy()
         ylims = [0, 0]
         for i, ax in enumerate(axs[0]):
-            component = model.construct_single_component(0, i).detach().numpy()
+            component = model.construct_single_component(0, i).detach().cpu().numpy()
             trimmed = data[(W[i] / W.sum(0)) > 0.4][:, times]
             sorted_trimmed = trimmed[np.argsort(W[i, (W[i] / W.sum(0)) > 0.4])][::-1]
             plot_dist(trimmed.reshape(-1, 200), ax=ax, color=colors[i], mode='std', times=conds[cond])
@@ -285,23 +285,31 @@ if __name__ == '__main__':
     logging.getLogger("lightning.pytorch.accelerators.cuda").setLevel(logging.WARNING)
     decoders = [SimpleDecoder(4, len(labels[2]), 5e-4) for _ in range(n_components[0])]
     for i, decoder in enumerate(decoders):
-        xi = model.construct_single_component(0, i).detach().cpu().numpy()
-        ls = xi[..., :200]
-        lm = xi[..., 200:400]
-        stacked = np.concatenate([ls, lm], axis=0).swapaxes(0, 1)
-        data_windowed = LabeledArray(windower(stacked, 20, 2).swapaxes(0, -1))
+        # xi = model.construct_single_component(0, i).detach().cpu().numpy().swapaxes(0, 1)
+        xi = neural_data_tensor.clone().detach().cpu().numpy().swapaxes(0, 1)
+        trimmed = xi[(W[i] / W.sum(0)) > 0.4]
+        sorted_trimmed = trimmed[np.argsort(W[i, (W[i] / W.sum(0)) > 0.4])][
+                         ::-1]
+        ls = sorted_trimmed[..., :200]
+        lm = sorted_trimmed[..., 200:400]
+        stacked = np.concatenate([ls, lm], axis=1)
+        data_windowed = LabeledArray(windower(stacked, 20, 2).swapaxes(0, -1))[::5]
         data_windowed.labels[2] = np.concatenate([labels[0], labels[0]])
         data_windowed.labels[1] = labels[1]
         # decoder = SimpleDecoder(4, xi.shape[1] * xi.shape[2], 5e-3)
         # train the decoder briefly
-        out = Parallel(n_jobs=1, verbose=40)(delayed(process_data)(
-            d, 4, n_folds, val_size, target_map, max_epochs) for d in
+        out = Parallel(n_jobs=6, verbose=40)(delayed(process_data)(
+            d, 2, n_folds, val_size, target_map, max_epochs) for d in
                                              data_windowed)
-        results[str(i)] = out
+        results[str(i)] = [o.tolist() for o in out]
 
     # %% plot the results
     fig, ax = plt.subplots(1, 1)
     for i, res in results.items():
-        plot = torch.stack(res).flatten(1).T
-        fig = plot_dist(plot.detach().numpy(), times=(-0.4, 1.4), ax=ax)
+        plot = torch.tensor(res).flatten(1).T
+        fig = plot_dist(plot.detach().cpu().numpy(), times=(-0.4, 1.4), ax=ax)
         fig.title.set_text("Decoding accuracy")
+
+    # import pickle
+    # with open('results2.pkl', 'wb') as f:
+    #     pickle.dump(results, f)
