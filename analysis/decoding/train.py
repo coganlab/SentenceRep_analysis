c463963a14c6dfc160d05e077b279f510f36ac49
Aaron Earle-Richardson <Jakdaxter31@gmail.com>
2024-10-03 16:37:12 -0400

    train

--- analysis/decoding/train.py
+++ analysis/decoding/train.py
@@ -10,6 +10,8 @@ from analysis.decoding.models import CNNTransformer, SimpleDecoder
 from analysis.grouping import GroupData
 import os
 from ieeg.calc.mat import Labels, LabeledArray
+from ieeg.viz.ensemble import plot_dist
+import pickle
 
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
 # %% Define data module
@@ -135,84 +137,90 @@ if __name__ == '__main__':
     # # Create a gridspec instance with 3 rows and 3 columns
     device = ('cuda' if torch.cuda.is_available() else 'cpu')
     sub = GroupData.from_intermediates("SentenceRep", fpath, folder='stats')
-    idx = sorted(list(sub.SM))
-    aud_slice = slice(0, 175)
-    reduced = sub[:, :, :, idx][:, ['aud_ls', 'aud_lm', 'aud_jl']]
-    # reduced = sub[:, :, :, idx][:, ['go_ls', 'go_lm']]
+    idxs = {'Auditory': sub.AUD, 'Sensory-Motor': sub.SM, 'Production': sub.PROD}
+    out = {}
+    colors = ['r', 'g', 'b']
 
-    reduced = reduced.nan_common_denom(True, 10, True)
-    aud = reduced.array['zscore']
+    for i, (name, idx) in enumerate(idxs.items()):
+        idx = sorted(list(idx))
+        aud_slice = slice(0, 175)
+        reduced = sub[:, :, :, idx][:, ['aud_ls', 'aud_lm']]
+        # reduced = sub[:, :, :, idx][:, ['go_ls', 'go_lm']]
 
-    idx = [i for i, l in enumerate(sub.array.labels[3]) if
-     l in reduced.array.labels[2]]
-    # transfer data to torch tensor
-    aud.labels[0] = Labels(aud.labels[0].replace("aud_", ""))
-    aud = LabeledArray(np.ascontiguousarray(aud.__array__()), aud.labels)
-    data = aud.combine((0,1)).combine((0,2)).dropna()
-    # aud_go = LabeledArray(np.ascontiguousarray(aud_go.__array__()), aud_go.labels)
-    # data = aud_go.combine((0, 1)).combine((0, 2)).__array__()
-    # del sub
+        reduced = reduced.nan_common_denom(True, 10, True)
+        aud = reduced.array['zscore']
 
-    stitched = np.hstack([sub.signif['aud_ls', :, aud_slice],
-                          # sub.signif['aud_lm', :, aud_slice],
-                          sub.signif['resp', :]])
+        idx = [i for i, l in enumerate(sub.array.labels[3]) if
+         l in reduced.array.labels[2]]
+        # transfer data to torch tensor
+        aud.labels[0] = Labels(aud.labels[0].replace("aud_", ""))
+        aud = LabeledArray(np.ascontiguousarray(aud.__array__()), aud.labels)
+        data = aud.combine((0,1)).combine((0,2)).dropna()
+        # aud_go = LabeledArray(np.ascontiguousarray(aud_go.__array__()), aud_go.labels)
+        # data = aud_go.combine((0, 1)).combine((0, 2)).__array__()
+        # del sub
 
-    neural_data_tensor = torch.from_numpy(
-        data / np.nanstd(data)).to(device, dtype=torch.float32).permute(1, 2, 0)
+        stitched = np.hstack([sub.signif['aud_ls', :, aud_slice],
+                              # sub.signif['aud_lm', :, aud_slice],
+                              sub.signif['resp', :]])
 
-    # %% Create model
-    # model parameters
-    in_channels = data.shape[0]
-    # num_classes = 4
-    d_model = 12
-    kernel_time = 50  # ms
-    kernel_size = int(kernel_time * fs / 1000)  # kernel length in samples
-    stride_time = 10 # ms
-    stride = int(stride_time * fs / 1000)  # stride length in samples
-    padding = 0
-    n_head = 4
-    num_layers = 2
-    dim_fc = 24
-    dropout = 0.3
-    # learning_rate = 1e-5
+        neural_data_tensor = torch.from_numpy(
+            data / np.nanstd(data)).to(device, dtype=torch.float32).permute(1, 2, 0)
 
-    # instantiate the model
-    # model = CNNTransformer(in_channels, num_classes, data.shape[1], kernel_size, stride, padding,
-    #                           n_head, num_layers, dim_fc, dropout, learning_rate)
+        # %% Create model
+        # model parameters
+        in_channels = data.shape[0]
+        # num_classes = 4
+        d_model = 12
+        kernel_time = 50  # ms
+        kernel_size = int(kernel_time * fs / 1000)  # kernel length in samples
+        stride_time = 10 # ms
+        stride = int(stride_time * fs / 1000)  # stride length in samples
+        padding = 0
+        n_head = 4
+        num_layers = 2
+        dim_fc = 24
+        dropout = 0.3
+        # learning_rate = 1e-5
 
-    # %% Train the model with kfold
-    # instantiate the trainer
-    n_folds = 5
-    max_epochs = 500
+        # instantiate the model
+        # model = CNNTransformer(in_channels, num_classes, data.shape[1], kernel_size, stride, padding,
+        #                           n_head, num_layers, dim_fc, dropout, learning_rate)
 
-    callbacks = [
-                 EarlyStopping(monitor='val_loss', patience=3, mode='min',min_delta=0.0001
-                               )]
-    trainer = L.Trainer(max_epochs=max_epochs,
-                        accelerator='cpu',
-                        callbacks=callbacks,
-                        logger=True,
-                        )
-    target_map = {'heat': 0, 'hut': 1, 'hot': 2, 'hoot': 3}
+        # %% Train the model with kfold
+        # instantiate the trainer
+        n_folds = 5
+        max_epochs = 500
+
+        callbacks = [
+                     EarlyStopping(monitor='val_loss', patience=3, mode='min',min_delta=0.0001
+                                   )]
+        trainer = L.Trainer(max_epochs=max_epochs,
+                            accelerator='cpu',
+                            callbacks=callbacks,
+                            logger=True,
+                            )
+        target_map = {'heat': 0, 'hut': 1, 'hot': 2, 'hoot': 3}
 
 
-    # %% train the model
+        # %% train the model
 
-    # out = process_data(data[..., :20], 1, n_folds, val_size, target_map, max_epochs, verbose=False)
+        # out = process_data(data[..., :20], 1, n_folds, val_size, target_map, max_epochs, verbose=False)
 
-    # %% windowed decoding
-    from analysis.decoding import windower
-    from joblib import Parallel, delayed
+        # %% windowed decoding
+        from analysis.decoding import windower
+        from joblib import Parallel, delayed
 
-    data_windowed = LabeledArray(windower(data, 20, 2).swapaxes(0, -1))
-    data_windowed.labels[1] = data.labels[0]
-    data_windowed.labels[2] = data.labels[1]
+        data_windowed = LabeledArray(windower(data, 20, 2).swapaxes(0, -1))[::4]
+        data_windowed.labels[1] = data.labels[0]
+        data_windowed.labels[2] = data.labels[1]
 
-    out = Parallel(n_jobs=4, verbose=40)(delayed(process_data)(
-        d, 10, n_folds, val_size, target_map, max_epochs) for d in data_windowed)
+        out[name] = Parallel(n_jobs=4, verbose=40)(delayed(process_data)(
+            d, 10, n_folds, val_size, target_map, max_epochs) for d in data_windowed)
 
+    with open('decoding_results_aud.pkl', 'wb') as f:
+        pickle.dump(out, f)
     # %% plot the results
-    from ieeg.viz.ensemble import plot_dist
     plot = torch.stack(out).flatten(1).T
     fig = plot_dist(plot.detach().numpy(), times=(-0.4, 1.4))
     fig.title.set_text("Decoding accuracy")
