ba00ea30211248b8513b54003f727250afeacaac
Aaronearlerichardson <jakdaxter31@gmail.com>
2024-12-05 10:10:20 -0500

    finish spec process

--- analysis/preprocess_spec.py
+++ analysis/preprocess_spec.py
@@ -1,7 +1,7 @@
 from ieeg.io import get_data, raw_from_layout
 from ieeg.navigate import crop_empty_data, outliers_to_nan, trial_ieeg
 from ieeg.timefreq.utils import wavelet_scaleogram, crop_pad
-from ieeg.calc import stats
+from ieeg.calc import stats, scaling
 from ieeg.viz.ensemble import chan_grid
 from ieeg.viz.parula import parula_map
 from joblib import Parallel, delayed
@@ -13,7 +13,7 @@ from tqdm import tqdm
 import numpy as np
 
 
-n_jobs = 6
+n_jobs = 11
 
 # %% check if currently running a slurm job
 HOME = os.path.expanduser("~")
@@ -40,6 +40,10 @@ for subj in subjects:
     data = []
     nperm = 10000
     spec_type = 'hilbert'
+    filename = os.path.join(layout.root, 'derivatives',
+                            'spec', spec_type, subj, f'start-tfr.h5')
+    base = mne.time_frequency.read_tfrs(filename).crop(-0.5, 0)
+    sig2 = base.get_data()
     for name, window in zip(
             ("start", "resp", "aud_ls", "aud_lm", "aud_jl", "go_ls", "go_lm", "go_jl"),
             ((-0.5, 0.5), (-1, 1), *((-0.5, 1.5),) * 6)):  # time-perm
@@ -48,8 +52,6 @@ for subj in subjects:
         filename = os.path.join(layout.root, 'derivatives',
                                     'spec', spec_type, subj, f'{name}-tfr.h5')
         epoch = mne.time_frequency.read_tfrs(filename)
-        if name == "start":
-            sig2 = epoch.get_data(tmin=window[0], tmax=0)
         sig1 = epoch.get_data(tmin=window[0], tmax=window[1])
 
         # time-perm
@@ -61,5 +63,24 @@ for subj in subjects:
 
         # Plot the Time-Frequency Clusters
         # --------------------------------
-        figs = chan_grid(mask, size=(20, 10), vmin=0, vmax=1,
-                         cmap=parula_map, show=False)
+        # figs = chan_grid(epoch_mask, size=(20, 10), vmin=0, vmax=1,
+        #                  cmap=parula_map, show=False)
+
+        power = scaling.rescale(epoch, base, 'mean', copy=True)
+        z_score = scaling.rescale(epoch, base, 'zscore', copy=True)
+
+        # Calculate the p-value
+        p_vals = mne.time_frequency.AverageTFRArray(epoch_mask.info, p_act, tmin=window[0])
+        # p_vals = epoch_mask.copy()
+        data.append((name, epoch_mask.copy(), power.copy(), z_score.copy(),
+                     p_vals.copy()))
+
+    for name, epoch_mask, power, z_score, p_vals in data:
+        power.save(save_dir + f"/{subj}_{name}_power-epo.h5", overwrite=True,
+                   fmt='double')
+        z_score.save(save_dir + f"/{subj}_{name}_zscore-epo.h5", overwrite=True,
+                     fmt='double')
+        epoch_mask.save(save_dir + f"/{subj}_{name}_mask-ave.h5", overwrite=True)
+        p_vals.save(save_dir + f"/{subj}_{name}_pval-ave.h5", overwrite=True)
+
+    base.save(save_dir + f"/{subj}_base-epo.h5", overwrite=True)
