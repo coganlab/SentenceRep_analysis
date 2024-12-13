5668d2293161f63c5e85a7a22d475b6978d3c347
Aaronearlerichardson <jakdaxter31@gmail.com>
2024-12-13 15:03:52 -0500

    change hilbert to multitaper again

--- analysis/check/hilbert_spec.py
+++ analysis/check/hilbert_spec.py
@@ -16,16 +16,18 @@ HOME = os.path.expanduser("~")
 if 'SLURM_ARRAY_TASK_ID' in os.environ.keys():
     LAB_root = os.path.join(HOME, "workspace", "CoganLab")
     layout = get_data("SentenceRep", root=LAB_root)
-    subjects = list(int(os.environ['SLURM_ARRAY_TASK_ID']))
+    subjects = layout.get(return_type="id", target="subject")
+    subject = int(os.environ['SLURM_ARRAY_TASK_ID'])
 else:  # if not then set box directory
     LAB_root = os.path.join(HOME, "Box", "CoganLab")
     layout = get_data("SentenceRep", root=LAB_root)
     subjects = layout.get(return_type="id", target="subject")
+    subject = 29
 
-n_jobs = 7
+n_jobs = -1
 
 for sub in subjects:
-    if int(sub[1:]) == 32:
+    if int(sub[1:]) != subject:
         continue
     # Load the data
     filt = raw_from_layout(layout.derivatives['notch'], subject=sub,
