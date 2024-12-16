7e4589ab11dd99580a89c4b16b3c1b2157980c4c
Aaronearlerichardson <jakdaxter31@gmail.com>
2024-12-16 17:38:38 -0500

    added some downsampling solutions

--- analysis/check/hilbert_spec.py
+++ analysis/check/hilbert_spec.py
@@ -2,12 +2,9 @@
 import mne.time_frequency
 
 from ieeg.io import get_data, raw_from_layout
-from ieeg.navigate import trial_ieeg, channel_outlier_marker, crop_empty_data,\
-    outliers_to_nan
-from ieeg.calc.scaling import rescale
+from ieeg.navigate import trial_ieeg, crop_empty_data, outliers_to_nan
 import os
 from ieeg.timefreq.utils import crop_pad
-from ieeg.timefreq.gamma import hilbert_spectrogram
 import numpy as np
 
 ## check if currently running a slurm job
@@ -22,13 +19,53 @@ else:  # if not then set box directory
     LAB_root = os.path.join(HOME, "Box", "CoganLab")
     layout = get_data("SentenceRep", root=LAB_root)
     subjects = layout.get(return_type="id", target="subject")
-    subject = 29
+    subject = 7
 
-n_jobs = -1
+def resample_tfr(tfr, sfreq, copy=False):
+    """Resample a TFR object to a new sampling frequency"""
+    if copy:
+        tfr = tfr.copy()
+    sfreq = int(sfreq)
+    o_sfreq = tfr.info["sfreq"]
+
+    tfr._data = downsample(tfr._data, sfreq, o_sfreq)
+    lowpass = tfr.info.get("lowpass")
+    lowpass = np.inf if lowpass is None else lowpass
+    with tfr.info._unlock():
+        tfr.info["lowpass"] = min(lowpass, sfreq / 2.0)
+        tfr.info["sfreq"] = float(sfreq)
+    new_times = downsample(tfr.times, sfreq, o_sfreq)
+    # adjust indirectly affected variables
+    tfr._set_times(new_times)
+    tfr._raw_times = tfr.times
+    tfr._update_first_last()
+    return tfr
+
+def downsample(arr: np.ndarray, new_sfreq: int, sfreq: int = None):
+    """Downsample an array"""
+    if sfreq is None:
+        sfreq = arr.shape[-1]
+    elif sfreq != arr.shape[-1]:
+        factor = arr.shape[-1] / sfreq
+        sfreq = int(round(sfreq * factor))
+        new_sfreq = int(round(new_sfreq * factor))
+
+    if new_sfreq > sfreq:
+        raise ValueError("New sampling frequency must be less than original")
+    elif new_sfreq == sfreq:
+        return arr
+    elif sfreq % new_sfreq != 0:
+        indices = np.linspace(0, sfreq - 1, new_sfreq)
+        indices = np.round(indices).astype(int)
+        return arr[..., indices]
+    else:
+        return arr[..., ::sfreq // new_sfreq]
+
+n_jobs = -2
 
 for sub in subjects:
-    if int(sub[1:]) != subject:
-        continue
+    # if int(sub[1:]) != subject:
+    #     continue
     # Load the data
     filt = raw_from_layout(layout.derivatives['notch'], subject=sub,
                            extension='.edf', desc='notch', preload=False)
@@ -65,16 +102,13 @@ for sub in subjects:
         trials = trial_ieeg(good, epoch, times, preload=True)
         outliers_to_nan(trials, outliers=10)
         freq = np.geomspace(60, 300, num=40)
-        kwargs = dict(average=False, n_jobs=-2, freqs=freq, return_itc=False,
-                      n_cycles=freq / 2, time_bandwidth=4, )
+        kwargs = dict(average=False, n_jobs=n_jobs, freqs=freq, return_itc=False,
+                      n_cycles=freq / 2, time_bandwidth=4)
 
-        spec= trials.compute_tfr(method="multitaper", **kwargs)
-        if int(spec.info['sfreq']) > 100:
-            new_sfreq = 100
-            decim = spec.info['sfreq'] // new_sfreq
-            offset = spec.info['sfreq'] % new_sfreq
-            spec.decimate(decim, offset)
+        spec = trials.compute_tfr(method="multitaper", **kwargs)
         del trials
+        if spec.sfreq > 100:
+            resample_tfr(spec, 100)
         crop_pad(spec, "0.5s")
         # if epoch == "Start":
         #     base = spec.copy().crop(-0.5, 0)
