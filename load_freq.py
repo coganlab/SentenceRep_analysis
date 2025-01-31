e61c5b08f721f0958ed6bf7111cc54010ec39b46
Aaronearlerichardson <jakdaxter31@gmail.com>
2025-01-31 12:35:42 -0500

    trying new view casting method 2

--- load_freq.py
+++ load_freq.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-import scipy.stats as st
 import mne
 
 from ieeg.io import get_data
@@ -28,11 +27,17 @@ def load_data(datatype: str, out_type: type | str = float, average: bool = True)
                     zscore_ave[key][k][f] = zscore_ave[key][k][f][..., :200]
     else:
         zscore_ave = combine(zscore, (0, 3))
-        for key in zscore_ave.keys():
-            for t in zscore_ave[key].keys():
-                for k in zscore_ave[key][t].keys():
-                    for f in zscore_ave[key][t][k].keys():
-                        zscore_ave[key][t][k][f] = zscore_ave[key][t][k][f][:, :200]
+
+        # for key in conditions := list(zscore_ave.keys()):
+        #     for t in tokens := list(zscore_ave[key].keys()):
+        #         for k in channels := list(zscore_ave[key][t].keys()):
+        #             for f in frequencies := list(zscore_ave[key][t][k].keys()):
+        #                 zscore_ave[key][t][k][f] = zscore_ave[key][t][k][f][..., :200]
+        # memmap = np.memmap('data.dat', dtype=out_type, mode='w+',
+        #                    shape=(len(conditions), len(tokens), len(channels), len(frequencies), *zscore_ave[key][t][k][f].shape))
+        # for i, key in enumerate(conditions):
+        #     memmap[i] = LabeledArray.from_dict(zscore_ave[key], dtype=out_type)
+
     del zscore
     return LabeledArray.from_dict(zscore_ave, dtype=out_type)
 
@@ -41,41 +46,45 @@ def average_tfr_channels(tfr: mne.time_frequency.tfr.AverageTFR):
     avgd_data = np.nanmean(tfr.data, axis=0, keepdims=True)
     return mne.time_frequency.AverageTFRArray(info, avgd_data, tfr.times, tfr.freqs)
 
-sigs = load_data("significance", out_type=bool)
-
-AUD, SM, PROD, sig_chans = group_elecs(sigs, sigs.labels[1], sigs.labels[0])
-
-SM = sorted(SM)
-AUD = sorted(AUD)
-PROD = sorted(PROD)
-sig_chans = sorted(sig_chans)
-
-zscores = load_data("zscore", "float16", False)
-
-times = np.linspace(-0.5, 1.5, 200)
-
-info = mne.create_info(ch_names=zscores.labels[1].tolist(), sfreq=100)
-events = np.array([[0 + 200 * i, 0, i] for i in range(7)])
-event_id = dict(zip(zscores.labels[0], range(7)))
-picked = mne.time_frequency.EpochsTFRArray(
-    info, sigs.__array__(), times, zscores.labels[2],
-    events=events, event_id=event_id, drop_log=tuple(() for _ in range(7)))
-
-# picked.average()n .plot(picks=0)
-# chan_grid(picked['aud_ls'].average(), yscale='log', vlim=(0, 1), cmap=parula_map)
-# all_spec = [picked['aud_ls'].pick_channels([pick]) for pick in picks]
-# for sub in subjects:
-#     picks = tuple(p for p in zscores.labels[1][AUD].tolist() if sub in p)
-#     all_avg = average_tfr_channels(picked['aud_ls'].pick_channels(picks).average())
-#     all_avg.plot('Average', cmap=parula_map, vlim=(0, 1), title=sub)
-# all_avg = average_tfr_channels(picked['go_ls'].pick_channels(picks).average())
-# all_avg.plot('Average', cmap=parula_map, vlim=(0, 1))
+loader = DataLoader(layout, conds, "zscore", False, 'stats_freq',
+                    '.h5')
+zscore = loader.load_dict(dtype="float16")
+zscore2 = combine(zscore, (0, 3))
+zscore = LabeledArray.from_dict(zscore2, dtype="float16")
+# zscores = load_data("zscore", "float16", False)
+# sigs = load_data("significance", out_type=bool)
 #
-# picks_old = (p.split('-') for p in picks)
-# picks = [f"D{int(p[0][1:])}-{p[1]}" for p in picks_old]
-from ieeg.viz.mri import plot_on_average
-br = None
-for i, idx in enumerate([SM, AUD, PROD]):
-    picks = [f"D{int(p[0][1:])}-{p[1]}" for p in (s.split("-") for s in zscores.labels[1][idx])]
-    rgb = [1 if j == i else 0 for j in range(3)]
-    br = plot_on_average(subjects, picks=picks, hemi='both', color=rgb, fig=br)
+# AUD, SM, PROD, sig_chans = group_elecs(sigs, sigs.labels[1], sigs.labels[0])
+#
+# SM = sorted(SM)
+# AUD = sorted(AUD)
+# PROD = sorted(PROD)
+# sig_chans = sorted(sig_chans)
+#
+# times = np.linspace(-0.5, 1.5, 200)
+#
+# info = mne.create_info(ch_names=zscores.labels[1].tolist(), sfreq=100)
+# events = np.array([[0 + 200 * i, 0, i] for i in range(7)])
+# event_id = dict(zip(zscores.labels[0], range(7)))
+# picked = mne.time_frequency.EpochsTFRArray(
+#     info, sigs.__array__(), times, zscores.labels[2],
+#     events=events, event_id=event_id, drop_log=tuple(() for _ in range(7)))
+#
+# # picked.average()n .plot(picks=0)
+# # chan_grid(picked['aud_ls'].average(), yscale='log', vlim=(0, 1), cmap=parula_map)
+# # all_spec = [picked['aud_ls'].pick_channels([pick]) for pick in picks]
+# # for sub in subjects:
+# #     picks = tuple(p for p in zscores.labels[1][AUD].tolist() if sub in p)
+# #     all_avg = average_tfr_channels(picked['aud_ls'].pick_channels(picks).average())
+# #     all_avg.plot('Average', cmap=parula_map, vlim=(0, 1), title=sub)
+# # all_avg = average_tfr_channels(picked['go_ls'].pick_channels(picks).average())
+# # all_avg.plot('Average', cmap=parula_map, vlim=(0, 1))
+# #
+# # picks_old = (p.split('-') for p in picks)
+# # picks = [f"D{int(p[0][1:])}-{p[1]}" for p in picks_old]
+# from ieeg.viz.mri import plot_on_average
+# br = None
+# for i, idx in enumerate([SM, AUD, PROD]):
+#     picks = [f"D{int(p[0][1:])}-{p[1]}" for p in (s.split("-") for s in zscores.labels[1][idx])]
+#     rgb = [1 if j == i else 0 for j in range(3)]
+#     br = plot_on_average(subjects, picks=picks, hemi='both', color=rgb, fig=br)
