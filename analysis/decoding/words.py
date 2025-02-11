d9c079dacbb54990122b4fca6876b70e2fa63f03
Aaronearlerichardson <jakdaxter31@gmail.com>
2025-02-11 17:18:00 -0500

    add gpu control, better verbose message, corrected trial concatenation

--- analysis/decoding/words.py
+++ analysis/decoding/words.py
@@ -9,17 +9,17 @@ from analysis.utils.plotting import plot_horizontal_bars
 from ieeg.calc.stats import time_perm_cluster
 from ieeg.decoding.decode import Decoder, plot_all_scores, get_scores
 from ieeg.viz.ensemble import plot_dist_bound
-from ieeg.io import dict_to_structured_array
 
 
 def score(categories, test_size, method, n_splits, n_repeats, sub, idxs,
-          conds, window_kwargs, scores_dict, shuffle=False):
-    decoder = Decoder(categories, n_splits=n_splits, n_repeats=n_repeats, explained_variance=test_size, da_type=method)
+          conds, window_kwargs, scores_dict, gpu=False, shuffle=False, **extra_kwargs):
+    decoder = Decoder(categories, n_splits=n_splits, n_repeats=n_repeats,
+                      explained_variance=test_size, da_type=method, **extra_kwargs)
     names = list(scores_dict.keys())
     while len(scores_dict) > 0:
         scores_dict.popitem()
-    for key, values in get_scores(sub.array['zscore'], decoder, idxs, conds, names, shuffle=shuffle, **window_kwargs):
-        print(key)
+    for values in get_scores(sub.array['zscore'], decoder, idxs, conds,
+                                  names, on_gpu=gpu, shuffle=shuffle, **window_kwargs):
         scores_dict[key] = values
     return scores_dict
 
@@ -40,38 +40,36 @@ if __name__ == '__main__':
     idxs = [list(idx) for idx in idxs]
     names = list(scores.keys())
     conds = [['aud_ls', 'aud_lm'], ['go_ls', 'go_lm'], 'resp']
-    window_kwargs = {'window': 20, 'obs_axs': 1, 'normalize': 'true', 'n_jobs': -3,
-                    'average_repetitions': False, 'step': 5}
+    window_kwargs = {'window': 20, 'obs_axs': 1, 'normalize': 'true', 'n_jobs': 1,
+                    'average_repetitions': False}
 
     # %% Time Sliding decoding for word tokens
 
     scores = score({'heat': 1, 'hoot': 2, 'hot': 3, 'hut': 4}, 0.8, 'lda', 5, 10, sub, idxs, conds,
                                 window_kwargs, scores,
-                                shuffle=False)
-    dict_to_structured_array(scores, 'true_scores.npy')
+                                shuffle=False, gpu=True)
+    np.savez('true_scores.npz', **scores)
     scores2 = score({'heat': 1, 'hoot': 2, 'hot': 3, 'hut': 4},
                                     0.8, 'lda', 5, 50, sub, idxs, conds,
                                     window_kwargs, scores2,
-                                    shuffle=True)
-    dict_to_structured_array(scores2, 'shuffle_score.npy')
+                                    shuffle=True, gpu=True)
+    np.savez('shuffle_score.npz', **scores2)
 
     # %% Plotting
     data_dir = ''
-    true_scores = np.load(data_dir + 'true_scores_freq.npy', allow_pickle=True)[0]
-    true_scores = {name: true_scores[name] for name in true_scores.dtype.names}
+    true_scores = dict(np.load(data_dir + 'true_scores.npz', allow_pickle=True))
 
     plots = {}
     for key, values in true_scores.items():
         if values is None:
             continue
         plots[key] = np.mean(values.T[np.eye(4).astype(bool)].T, axis=2)
-    fig, axs = plot_all_scores(plots, conds, {n: i for n, i in zip(names[:-1], idxs[:-1])}, colors, "Word Decoding")
+    fig, axs = plot_all_scores(plots, conds, {n: i for n, i in zip(names, idxs)}, colors, "Word Decoding")
 
 
     # %% Time Sliding decoding significance
 
-    shuffle_score = np.load(data_dir + 'shuffle_score_brunner.npy', allow_pickle=True)[0]
-    shuffle_score = {name: shuffle_score[name] for name in shuffle_score.dtype.names}
+    shuffle_score = dict(np.load(data_dir + 'shuffle_score.npz.', allow_pickle=True))
     signif = {}
     for cond, score in true_scores.items():
         true = np.mean(score.T[np.eye(4).astype(bool)].T, axis=2)
