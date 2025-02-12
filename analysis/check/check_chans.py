faf6519881ade0ffa9758eb154aae61df66356a0
Aaronearlerichardson <jakdaxter31@gmail.com>
2025-02-12 12:11:37 -0500

    fix

--- analysis/check/check_chans.py
+++ analysis/check/check_chans.py
@@ -3,9 +3,11 @@ import mne.time_frequency
 from ieeg.viz.ensemble import chan_grid
 from ieeg.viz.parula import parula_map
 from ieeg.io import get_data, update, get_bad_chans
+from ieeg.calc.scaling import rescale
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats as st
 
 ## check if currently running a slurm job
 HOME = os.path.expanduser("~")
@@ -14,7 +16,7 @@ if 'SLURM_ARRAY_TASK_ID' in os.environ.keys():
     subject = int(os.environ['SLURM_ARRAY_TASK_ID'])
 else:  # if not then set box directory
     LAB_root = os.path.join(HOME, "Box", "CoganLab")
-    subject = 30
+    subject =53
 
 
 # Load the data
@@ -24,12 +26,14 @@ subjects = layout.get(return_type="id", target="subject")
 conds = ["start", "resp", "aud_ls", "aud_lm", "aud_jl", "go_jl", "go_ls", "go_lm"]
 
 for subj in subjects:
-    # if int(subj[1:]) <= subject:
-    #     continue
+    if int(subj[1:]) != subject:
+        continue
     for cond in conds:
-        spec_type = 'hilbert'
+        spec_type = 'wavelet_test'
         filename = os.path.join(layout.root, 'derivatives',
                                 'spec', spec_type, subj, f'{cond}-tfr.h5')
+        # filename = os.path.join(layout.root, 'derivatives', 'stats_freq',
+        #                         f'{subj}_{cond}_{spec_type}-tfr.h5')
         try:
             spec = mne.time_frequency.read_tfrs(filename)
             if isinstance(spec, mne.time_frequency.tfr.EpochsTFR):
@@ -39,14 +43,20 @@ for subj in subjects:
         except OSError:
             print(f"Skipping {subj} {cond}")
             continue
-        info_file = os.path.join(layout.root, spec_a.info['subject_info']['files'][0])
-        all_bad = get_bad_chans(info_file)
-        spec_a.info.update(bads=[b for b in all_bad if b in spec_a.ch_names])
+        if cond == 'start':
+            base = spec_a.copy().crop(tmin=-0.5, tmax=0)
+
+        # info_file = os.path.join(layout.root, spec_a.info['subject_info']['files'][0])
+        # all_bad = get_bad_chans(info_file)
+        # spec_a.info.update(bads=[b for b in all_bad if b in spec_a.ch_names])
+        # rescale(spec_a, base, mode='ratio', copy=False)
+        # spec_a._data = np.where(spec_a._data > 0.9999, 0.9999, spec_a._data)
+        # spec_a._data = st.norm.ppf(1 - spec_a._data)
 
         ## plotting
         figs = chan_grid(spec_a, size=(20, 10),
-                         vlim=(-0.5, 1),
-                         cmap=parula_map, show=False, yscale='log')
+                         vlim=(0., 1.),
+                         cmap=parula_map, show=False)
         fig_path = os.path.join(layout.root, 'derivatives', 'spec', spec_type, 'figs')
         if not os.path.exists(fig_path):
             os.makedirs(fig_path)
